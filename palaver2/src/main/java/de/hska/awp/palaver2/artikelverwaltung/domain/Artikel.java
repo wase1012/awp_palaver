aec7fd1392050afd7d99a6c470d95c0316d9383d
wase1012 <wase1012@hs-karlsruhe.de>
2013-04-17 11:37:14 +0200

    Artikel

---
+++ palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Artikel.java
@@ -0,0 +1,183 @@
+/**
+ * Created by Sebastian Walz
+ * 16.04.2013 15:32:46
+ */
+package de.hska.awp.palaver2.artikelverwaltung.domain;
+
+import de.hska.awp.palaver2.bean.Bean;
+
+public class Artikel implements Bean
+{
+	private Long			id;
+	private String			name;
+	private float			preis;
+//	private Lieferant		lieferant
+//	private Mengeneinheit	mengeneinheit		
+	private Boolean			bio;		
+//	private Kategorie		kategorie;
+	private String			artikelnummer;
+	private Boolean			standard;
+	private Boolean			grundbedarf;
+	private Integer			durchschnitt;
+	private float			bestellgroesse;
+	
+	@Override
+	public Long getId()
+	{
+		return id;
+	}
+
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+
+	/**
+	 * @return the preis
+	 */
+	public float getPreis()
+	{
+		return preis;
+	}
+
+
+	/**
+	 * @param preis the preis to set
+	 */
+	public void setPreis(float preis)
+	{
+		this.preis = preis;
+	}
+
+
+	/**
+	 * @return the bio
+	 */
+	public Boolean getBio()
+	{
+		return bio;
+	}
+
+
+	/**
+	 * @param bio the bio to set
+	 */
+	public void setBio(Boolean bio)
+	{
+		this.bio = bio;
+	}
+
+
+	/**
+	 * @return the artikelnummer
+	 */
+	public String getArtikelnummer()
+	{
+		return artikelnummer;
+	}
+
+
+	/**
+	 * @param artikelnummer the artikelnummer to set
+	 */
+	public void setArtikelnummer(String artikelnummer)
+	{
+		this.artikelnummer = artikelnummer;
+	}
+
+
+	/**
+	 * @return the standard
+	 */
+	public Boolean getStandard()
+	{
+		return standard;
+	}
+
+
+	/**
+	 * @param standard the standard to set
+	 */
+	public void setStandard(Boolean standard)
+	{
+		this.standard = standard;
+	}
+
+
+	/**
+	 * @return the grundbedarf
+	 */
+	public Boolean getGrundbedarf()
+	{
+		return grundbedarf;
+	}
+
+
+	/**
+	 * @param grundbedarf the grundbedarf to set
+	 */
+	public void setGrundbedarf(Boolean grundbedarf)
+	{
+		this.grundbedarf = grundbedarf;
+	}
+
+
+	/**
+	 * @return the durchschnitt
+	 */
+	public Integer getDurchschnitt()
+	{
+		return durchschnitt;
+	}
+
+
+	/**
+	 * @param durchschnitt the durchschnitt to set
+	 */
+	public void setDurchschnitt(Integer durchschnitt)
+	{
+		this.durchschnitt = durchschnitt;
+	}
+
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+
+
+	/**
+	 * @return the bestellgroesse
+	 */
+	public float getBestellgroesse()
+	{
+		return bestellgroesse;
+	}
+
+
+	/**
+	 * @param bestellgroesse the bestellgroesse to set
+	 */
+	public void setBestellgroesse(float bestellgroesse)
+	{
+		this.bestellgroesse = bestellgroesse;
+	}
+
+}
