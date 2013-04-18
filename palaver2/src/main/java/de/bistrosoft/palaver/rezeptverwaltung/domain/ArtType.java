1da471e82d5eb39f4037d4ab3764181cba047d6d
Jan Lauinger <laja1011@hs-karlsruhe.de>
2013-04-18 21:00:39 +0200

    enum ArtType BEILAGE, Hauptgericht, ...

---
+++ palaver2/src/main/java/de/bistrosoft/palaver/rezeptverwaltung/domain/ArtType.java
@@ -0,0 +1,23 @@
+/**
+ * 
+Jan Lauinger
+*
+18.04.2013
+*
+ */
+package de.bistrosoft.palaver.rezeptverwaltung.domain;
+
+/**
+ * @author Jan Lauinger
+ *
+ */
+public enum ArtType {
+	
+	BEILAGE, 
+	HAUPTGERICHT,
+	KUCHEN,
+	SALAT,
+	SUPPE;
+		
+
+}
