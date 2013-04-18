ccb91ff0ab4afbd9ee87e4f101d8f1d29510a94c
tuph1011 <tuph1011@hs-karlsruhe.de>
2013-04-18 15:21:19 +0200

    hash equal toString inserted

--- palaver2/src/main/java/de/hska/awp/palaver2/nachrichtenverwaltung/domain/Nachricht.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/nachrichtenverwaltung/domain/Nachricht.java
@@ -36,6 +36,52 @@ public class Nachricht implements Bean {
 		return id;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Nachricht [id=" + id + ", nachricht=" + nachricht + "]";
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result
+				+ ((nachricht == null) ? 0 : nachricht.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Nachricht other = (Nachricht) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nachricht == null) {
+			if (other.nachricht != null)
+				return false;
+		} else if (!nachricht.equals(other.nachricht))
+			return false;
+		return true;
+	}
+
 
 	
 }
