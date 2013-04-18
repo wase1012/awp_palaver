c9dad53cf5e888aeaad09a7a2cd2ac39ee098b68
Jan Lauinger <laja1011@hs-karlsruhe.de>
2013-04-18 20:39:27 +0200

    enum GeschmackType exotisch, mediterran, ...

---
+++ palaver2/src/main/java/de/bistrosoft/palaver/rezeptverwaltung/domain/GeschmackType.java
@@ -0,0 +1,37 @@
+package de.bistrosoft.palaver.rezeptverwaltung.domain;
+
+public enum GeschmackType {
+	
+	EXOTISCH(0), 
+	DEFTIG(1),
+	KLASSISCH(2),
+	MEDITERRAN(3);
+		
+	private int value;
+		
+	private GeschmackType(int value) {
+		this.value = value;
+	}
+		
+	public static GeschmackType valueOf(int value) {
+		switch (value) {
+			case 0:
+				return EXOTISCH;
+			case 1:
+				return DEFTIG;
+			case 2:
+				return KLASSISCH;
+			case 3:
+				return MEDITERRAN;
+			default:
+				return null;
+		}
+	}
+		
+	public int value() {
+		return value;
+	}
+}
+
+
+
