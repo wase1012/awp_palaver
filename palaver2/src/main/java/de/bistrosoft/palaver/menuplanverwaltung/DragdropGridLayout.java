dfd67c4a9c49b25ec7dd6aaa25f36a237967b028
eikebecher <mail@ebecher.de>
2013-04-20 14:08:43 -0700

    Menüplan erweitert

--- palaver2/src/main/java/de/bistrosoft/palaver/menuplanverwaltung/DragdropGridLayout.java
+++ palaver2/src/main/java/de/bistrosoft/palaver/menuplanverwaltung/DragdropGridLayout.java
@@ -2,19 +2,24 @@ package de.bistrosoft.palaver.menuplanverwaltung;
 
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 import fi.jasoft.dragdroplayouts.DDGridLayout;
 import fi.jasoft.dragdroplayouts.client.ui.LayoutDragMode;
 import fi.jasoft.dragdroplayouts.drophandlers.DefaultGridLayoutDropHandler;
+import fi.jasoft.dragdroplayouts.interfaces.DragFilter;
 
 @SuppressWarnings("serial")
 public class DragdropGridLayout extends CustomComponent{
 
-    private static final int ROWS = 4;
-    private static final int COLUMNS = 4;
+    private static final int ROWS = 8;
+    private static final int COLUMNS = 5;
    
     public DragdropGridLayout() {
     setCaption("Grid layout");
@@ -28,8 +33,11 @@ public class DragdropGridLayout extends CustomComponent{
 
     // Create a drag and droppable grid layout
     final DDGridLayout layout = new DDGridLayout(COLUMNS, ROWS);
-    layout.setWidth("400px");
-    layout.setHeight("100%");
+    int width = COLUMNS*150; 
+    int height = ROWS*100;
+    layout.setWidth(width+"px");
+    layout.setHeight(height+"px");
+//    layout.addStyleName("gridLayoutBorders");
    
     // Only allow dropping in the center of the grid layout cell
     layout.setComponentHorizontalDropRatio(0);
@@ -43,14 +51,45 @@ public class DragdropGridLayout extends CustomComponent{
 
     // Enable dropping components
     layout.setDropHandler(new DefaultGridLayoutDropHandler());
+    
+    // Limit dragging to only buttons
+    layout.setDragFilter(new DragFilter() {      
+    public boolean isDraggable(Component component) {  
+    	return component instanceof Label;
+        }
+            });
    
-    for (int row = 0; row < ROWS; row++) {
+    for (int row = 2; row < ROWS; row++) {
         for (int col = 0; col < COLUMNS; col++) {
-            if (row == 0 || row == ROWS-1 || col == 0 || col == COLUMNS-1) {
-                Button btn = new Button("Button");
+                Button btn = new Button("ADD");
+                btn.addClickListener(new ClickListener() {
+					
+					@Override
+					public void buttonClick(ClickEvent event) {
+						
+						for (int row = 0; row < ROWS; row++) {
+					        for (int col = 0; col < COLUMNS; col++) {
+					        	Button tmp = event.getButton();
+					        	if(tmp.equals(layout.getComponent(col, row))) {
+					        		layout.removeComponent(tmp);
+					        		
+					        		WinSelectMenue window = new WinSelectMenue();
+					        		UI.getCurrent().addWindow(window);
+					        		window.setModal(true);
+					        		
+					        		Label lbTmp = new Label("Pommes mit Schnitzel");
+					        		lbTmp.setWidth("140px");
+					        		layout.addComponent(lbTmp,col,row);	
+					                layout.setComponentAlignment(lbTmp, Alignment.MIDDLE_CENTER);
+					        	}
+					        }
+						}
+						
+					}
+				});
+                
                 layout.addComponent(btn, col, row);
                 layout.setComponentAlignment(btn, Alignment.MIDDLE_CENTER);
-            }
         }
     }
 }
---
+++ palaver2/src/main/java/de/bistrosoft/palaver/menuplanverwaltung/WinSelectMenue.java
@@ -0,0 +1,9 @@
+package de.bistrosoft.palaver.menuplanverwaltung;
+
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class WinSelectMenue extends Window {
+	
+
+}
