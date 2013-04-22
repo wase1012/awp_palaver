af60ee92e23f436185a79df7d00857609693b1fd
tuph1011 <tuph1011@hs-karlsruhe.de>
2013-04-22 16:16:56 +0200

    Mitarbeiter auf Long NachrichtTest erweitert

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/NachrichtTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/NachrichtTest.java
@@ -1,8 +1,40 @@
-/**
- * 
- */
+package de.hska.awp.palaver2.testklassen.domain;
+
+import org.junit.Test;
+
+import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
+import de.hska.awp.palaver2.mitarbeiterverwaltung.domain.Mitarbeiter;
+import de.hska.awp.palaver2.nachrichtenverwaltung.domain.Nachricht;
+import de.hska.awp.palaver2.util.AbstractTest;
+
 /**
  * @author PhilippT
  *
  */
-package de.hska.awp.palaver2.testklassen.domain;
+
+public class NachrichtTest extends AbstractTest {
+	
+
+	private static final String test_nachricht = "Ich bin die Testnachricht";	
+	
+	@Test
+	public void createNachrichten() {
+		
+		final Nachricht n = new Nachricht();
+		n.setNachricht(test_nachricht);
+		
+    	Long sid = Long.valueOf(1);
+    	Mitarbeiter sender = em.find(Mitarbeiter.class, sid);
+    	n.setMitarbeiterBySenderFk(sender);
+    	
+    	Long eid = Long.valueOf(2);   	
+    	Mitarbeiter empfaenger = em.find(Mitarbeiter.class, eid);
+		n.setMitarbeiterByEmpfaengerFk(empfaenger);    	
+    	
+    	em.persist(n);
+    	em.getTransaction().commit();
+		
+	}
+			
+	
+}
