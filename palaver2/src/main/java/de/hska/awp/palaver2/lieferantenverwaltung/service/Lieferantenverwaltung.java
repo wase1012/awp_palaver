929d847d2b54e7394f87ae12af7c0e15ab2ba3a4
bach1014 <barth_chris@hotmail.com>
2013-04-22 18:46:53 +0200

    Dao für Lieferantenverwaltung erstellt.

--- palaver2/src/main/java/de/hska/awp/palaver2/lieferantenverwaltung/service/Lieferantenverwaltung.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/lieferantenverwaltung/service/Lieferantenverwaltung.java
@@ -4,23 +4,30 @@
  */
 package de.hska.awp.palaver2.lieferantenverwaltung.service;
 
+import java.io.Serializable;
+
 import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
 
 /**
  * @author bach1014
  *
  */
-public class Lieferantenverwaltung {
+public class Lieferantenverwaltung implements Serializable {
+	
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -5676269878706221454L;
+
+	private LieferantenverwaltungDao dao;
 	
 	public Lieferant createLieferant(Lieferant lieferant) {
 		
 		if (lieferant == null) {
 			return lieferant;
 		}
-	
 		
-		//TODO Überprüfung, ob es den Lieferanten schon gibt, notfalls exceptions werfen	
-		//lieferant = (Lieferant) em.persist(lieferant);
+		lieferant = (Lieferant) dao.create(lieferant);
 		
 		return lieferant;
 	}
---
+++ palaver2/src/main/java/de/hska/awp/palaver2/lieferantenverwaltung/service/LieferantenverwaltungDao.java
@@ -0,0 +1,17 @@
+/**
+ * Created by bach1014
+ * 22.04.2013 - 17:43:09
+ */
+package de.hska.awp.palaver2.lieferantenverwaltung.service;
+
+import de.hska.awp.palaver2.util.Dao;
+
+/**
+ * @author bach1014
+ *
+ */
+public class LieferantenverwaltungDao extends Dao {
+		private static final long serialVersionUID = -6166455307123578665L;
+
+		
+}
