c2410f9e505b742db7fb7bd9a05388f431d60b92
tuph1011 <tuph1011@hs-karlsruhe.de>
2013-04-23 11:59:09 +0200

    sinnvollerer test

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/NachrichtTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/NachrichtTest.java
@@ -3,9 +3,12 @@ package de.hska.awp.palaver2.testklassen.domain;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import org.junit.Test;
 
-import de.hska.awp.palaver2.lieferantenverwaltung.domain.Ansprechpartner;
 import de.hska.awp.palaver2.mitarbeiterverwaltung.domain.Mitarbeiter;
 import de.hska.awp.palaver2.nachrichtenverwaltung.domain.Nachricht;
 import de.hska.awp.palaver2.util.AbstractTest;
@@ -23,7 +26,7 @@ public class NachrichtTest extends AbstractTest {
     @Test
     public void findNachrichtById() {
     	
-    	final Long id = Long.valueOf("1");
+    	Long id = Long.valueOf(2);
 
 		Nachricht nachricht = em.find(Nachricht.class, id);
 
@@ -48,6 +51,24 @@ public class NachrichtTest extends AbstractTest {
     	em.getTransaction().commit();
 		
 	}
+	
+    @Test
+    public void deleteNachricht() {
+    	
+    	Long id = Long.valueOf(1);
+    	
+    	final TypedQuery<Nachricht> query = em.createNamedQuery(
+				Nachricht.FIND_Nachricht_BY_ID, Nachricht.class);
+		query.setParameter(Nachricht.PARAM_ID, id);
+		final List<Nachricht> nachricht = query.getResultList();
+    	
+    	for(int i = 0; i < nachricht.size(); i++){
+    		Nachricht nr = nachricht.get(i);
+    		em.remove(nr);
+	    	
+    	}
+    	em.getTransaction().commit();
+    }
 			
 	
 }
