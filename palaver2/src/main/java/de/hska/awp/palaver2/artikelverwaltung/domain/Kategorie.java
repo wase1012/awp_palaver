1f80873fcb29c826856f14ddb29a1daee115d7aa
bomi1012 <bomi1012@hs-karlsruhe.de>
2013-04-23 17:39:20 +0200

    Domain klass Kategorie

--- palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Kategorie.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Kategorie.java
@@ -12,45 +12,106 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "kategorie", catalog = "palaver")
-public class Kategorie implements java.io.Serializable 
-{
-	private static final long 			serialVersionUID = -4647006694762094910L;
-	
+public class Kategorie implements java.io.Serializable {
+	private static final long serialVersionUID = -4647006694762094910L;
+
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
 	private Integer id;
-	
+
 	@Column(name = "name", nullable = false, length = 45)
 	private String name;
 
-	public Kategorie() 
-	{
+	/**
+	 * Standardkonstruktor
+	 */
+	public Kategorie() {
 		super();
 	}
 
-	public Kategorie(String name) 
-	{
+	/**
+	 * Konstruktor
+	 * 
+	 * @param name
+	 *            Kategoriename
+	 */
+	public Kategorie(String name) {
 		this.name = name;
 	}
 
-	public Integer getId() 
-	{
+	/**
+	 * getId + setId
+	 * 
+	 * @return KategorieId
+	 */
+	public Integer getId() {
 		return this.id;
 	}
 
-	public void setId(Integer id) 
-	{
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
-	public String getName() 
-	{
+	/**
+	 * getName + setName
+	 * 
+	 * @return KategorieName
+	 */
+	public String getName() {
 		return this.name;
 	}
 
-	public void setName(String name) 
-	{
+	public void setName(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Kategorie [id=" + id + ", name=" + name + "]";
+	}
+
+	/**
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	/**
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kategorie other = (Kategorie) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+
+	
 }
