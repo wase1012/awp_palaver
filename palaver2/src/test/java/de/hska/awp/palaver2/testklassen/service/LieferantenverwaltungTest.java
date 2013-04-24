43fd1e62ae80204ca76016b981bb9d5437d9d92c
bach1014 <barth_chris@hotmail.com>
2013-04-24 14:32:48 +0200

    Attribute an Domainklassen geändert. Bitte DB neu aufsetzten, da es sonst zu Fehlern kommt.

---
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/LieferantenverwaltungTest.java
@@ -0,0 +1,40 @@
+/**
+ * Created by bach1014
+ * 24.04.2013 - 12:05:27
+ */
+package de.hska.awp.palaver2.testklassen.service;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
+import de.hska.awp.palaver2.lieferantenverwaltung.service.Lieferantenverwaltung;
+
+/**
+ * @author bach1014
+ *
+ */
+public class LieferantenverwaltungTest {
+
+	Lieferantenverwaltung lv;
+	
+	@Test
+    public void findLieferantByName() {
+    	
+    	final String name = "Edeka";
+	
+    		// When
+    		final List<Lieferant> liefantlist = lv.findLieferantByName(name);
+    		
+    		// Then
+    		for (Lieferant k : liefantlist) {
+    			assertThat(k.getName(), is(name));
+    		}
+    	}
+
+    }
+	
