dfe54cd626806459fca509e970de11595edcae09
bomi1012 <bomi1012@hs-karlsruhe.de>
2013-04-24 09:42:23 +0200

    ID long

--- palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Kategorie.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Kategorie.java
@@ -30,7 +30,7 @@ public class Kategorie implements java.io.Serializable {
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
-	private Integer id;
+	private Long id;
 
 	@Column(name = "name", nullable = false, length = 45)
 	private String name;
@@ -70,11 +70,11 @@ public class Kategorie implements java.io.Serializable {
 	@Id
 	@GeneratedValue(strategy = IDENTITY)
 	@Column(name = "id", unique = true, nullable = false)
-	public Integer getId() {
+	public Long getId() {
 		return this.id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
