52f477303b09594751c8cb4490a180eb3cd4caae
Eike Becher <mail@ebecher.de>
2013-04-25 16:44:20 +0200

    Klasse Week für Menüplan

---
+++ palaver2/src/main/java/de/bistrosoft/palaver/util/Week.java
@@ -0,0 +1,22 @@
+package de.bistrosoft.palaver.util;
+
+public class Week {
+	private int week;
+	private int year;
+	
+	public int getWeek() {
+		return week;
+	}
+	
+	public void setWeek(int week) {
+		this.week = week;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+}
