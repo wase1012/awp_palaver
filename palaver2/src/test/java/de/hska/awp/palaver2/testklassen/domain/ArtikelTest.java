d887d6da889d98cc281067ad09da1fbf56eb100b
wase1012 <wase1012@hs-karlsruhe.de>
2013-04-26 17:43:28 +0200

    ArtikelDAO fixed

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/ArtikelTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/ArtikelTest.java
@@ -13,9 +13,12 @@ import java.util.List;
 import org.junit.Test;
 
 import de.hska.awp.palaver2.artikelverwaltung.domain.Artikel;
+import de.hska.awp.palaver2.artikelverwaltung.domain.Kategorie;
+import de.hska.awp.palaver2.artikelverwaltung.domain.Mengeneinheit;
 import de.hska.awp.palaver2.data.ArtikelDAO;
 import de.hska.awp.palaver2.data.ConnectException;
 import de.hska.awp.palaver2.data.DAOException;
+import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
 import de.hska.awp.palaver2.util.AbstractTest;
 
 public class ArtikelTest extends AbstractTest
@@ -37,5 +40,29 @@ public class ArtikelTest extends AbstractTest
 		}
 		assertThat(list.isEmpty(), is(false));
 		assertThat(exception, is(false));
+	}
+	
+	@Test
+	public void getArtikelById() throws ConnectException, DAOException, SQLException
+	{
+		Long id = 1L;
+		
+		Artikel artikel = artikelDao.getArtikelById(id);
+		
+		assertThat(artikel.getId(), is(id));
+	}
+	
+	@Test
+	public void createArtikel() throws ConnectException, DAOException
+	{
+		Mengeneinheit mengeneinheit = new Mengeneinheit();
+		mengeneinheit.setId(1L);
+		Kategorie kategorie = new Kategorie(1L, "Test");
+		Lieferant lieferant = new Lieferant();
+		lieferant.setId(1L);
+		
+		Artikel artikel = new Artikel(mengeneinheit, kategorie, lieferant, "Test", "Test", 0.0, 0.0F, true, true, false, 0, true);
+		
+		artikelDao.createArtikel(artikel);
 	}
 }
