0bcf06b752f1e98436febeb9b11c4cfa98356f1f
wase1012 <wase1012@hs-karlsruhe.de>
2013-04-26 14:18:33 +0200

    ArtikelDAO

--- palaver2/src/main/java/de/hska/awp/palaver2/data/ArtikelDAO.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/data/ArtikelDAO.java
@@ -5,6 +5,7 @@ package de.hska.awp.palaver2.data;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +17,8 @@ import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
 public class ArtikelDAO extends AbstractDAO
 {	
 	private final static String			GET_ALL_ARTIKLES = "SELECT * FROM artikel";
+	private final static String			GET_ARTIKEL_BY_ID = "SELECT * FROM artikel where id = {0}";
+	private final static String			GET_ARTIKEL_BY_NAME = "SELECT * FROM artikel where name = '{0}'";
 	
 	public ArtikelDAO()
 	{
@@ -27,6 +30,60 @@ public class ArtikelDAO extends AbstractDAO
 		List<Artikel> list = new ArrayList<Artikel>();
 		
 		ResultSet set = get(GET_ALL_ARTIKLES);
+		
+		while(set.next())
+		{
+			list.add(new Artikel(set.getLong("id"),
+								new Mengeneinheit(),
+								new Kategorie(),
+								new Lieferant(),
+								set.getString("artikelnummer"),
+								set.getString("name"),
+								set.getDouble("bestellgroesse"),
+								set.getFloat("preis"),
+								set.getBoolean("bio"),
+								set.getBoolean("standard"),
+								set.getBoolean("grundbedarf"),
+								set.getInt("durchschnitt"),
+								set.getBoolean("")
+								));
+		}
+		
+		return list;
+	}
+	
+	public Artikel getArtikelById(Long id) throws ConnectException, DAOException, SQLException
+	{
+		Artikel result = null;
+		
+		ResultSet set = get(MessageFormat.format(GET_ARTIKEL_BY_ID, id));
+		
+		while(set.next())
+		{
+			result = new Artikel(set.getLong("id"),
+								new Mengeneinheit(),
+								new Kategorie(),
+								new Lieferant(),
+								set.getString("artikelnummer"),
+								set.getString("name"),
+								set.getDouble("bestellgroesse"),
+								set.getFloat("preis"),
+								set.getBoolean("bio"),
+								set.getBoolean("standard"),
+								set.getBoolean("grundbedarf"),
+								set.getInt("durchschnitt"),
+								set.getBoolean("")
+								);
+		}
+		
+		return result;
+	}
+	
+	public List<Artikel> getArtikelByName(String name) throws ConnectException, DAOException, SQLException
+	{
+		List<Artikel> list = new ArrayList<Artikel>();
+		
+		ResultSet set = get(MessageFormat.format(GET_ARTIKEL_BY_NAME, name));
 		
 		while(set.next())
 		{
