334978d9fdd25cd774d909aed2c774894bce8e2b
tuph1011 <tuph1011@hs-karlsruhe.de>
2013-04-27 14:09:29 +0200

    setid wieder drin

--- palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Mengeneinheit.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/artikelverwaltung/domain/Mengeneinheit.java
@@ -25,6 +25,10 @@ public class Mengeneinheit implements java.io.Serializable {
 	public Long getId() {
 		return this.id;
 	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
 
 	public String getName() {
 		return this.name;
