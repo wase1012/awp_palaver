2c62f19c85875042106815c3e83d48e08e24a82f
wase1012 <wase1012@hs-karlsruhe.de>
2013-04-28 12:41:35 +0200

    Filterbar

--- palaver2/src/main/java/de/hska/awp/palaver2/gui/view/ArtikelAnzeigen.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/gui/view/ArtikelAnzeigen.java
@@ -9,13 +9,17 @@ import java.sql.SQLException;
 import org.tepi.filtertable.FilterTable;
 
 import com.vaadin.data.util.BeanItemContainer;
-import com.vaadin.ui.Table;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
 
 import de.hska.awp.palaver2.artikelverwaltung.domain.Artikel;
 import de.hska.awp.palaver2.artikelverwaltung.service.Artikelverwaltung;
 import de.hska.awp.palaver2.data.ConnectException;
 import de.hska.awp.palaver2.data.DAOException;
+import de.hska.awp.palaver2.util.IConstants;
 import de.hska.awp.palaver2.util.customFilter;
 import de.hska.awp.palaver2.util.customFilterDecorator;
 
@@ -24,15 +28,20 @@ public class ArtikelAnzeigen extends VerticalLayout
 {
 	private FilterTable		table;
 	
+	private Button			showFilter;
+	
 	public ArtikelAnzeigen()
 	{
 		super();
 		
 		this.setSizeFull();
 		this.setMargin(true);
+		
+		showFilter = new Button(IConstants.BUTTON_SHOW_FILTER);
+		
 		table = new FilterTable();
 		table.setSizeFull();
-		table.setFilterBarVisible(true);
+		table.setFilterBarVisible(false);
 		table.setFilterGenerator(new customFilter());
 		table.setFilterDecorator(new customFilterDecorator());
 		
@@ -50,6 +59,28 @@ public class ArtikelAnzeigen extends VerticalLayout
 			e.printStackTrace();
 		}	
 		
+		this.addComponent(showFilter);
+		this.setComponentAlignment(showFilter, Alignment.MIDDLE_RIGHT);
 		this.addComponent(table);
+		this.setExpandRatio(table, 1);
+		
+		showFilter.addClickListener(new ClickListener()
+		{
+			@Override
+			public void buttonClick(ClickEvent event)
+			{
+				if (table.isFilterBarVisible())
+				{
+					table.setFilterBarVisible(false);
+					table.resetFilters();
+					showFilter.setCaption(IConstants.BUTTON_SHOW_FILTER);
+				}
+				else
+				{
+					table.setFilterBarVisible(true);
+					showFilter.setCaption(IConstants.BUTTON_HIDE_FILTER);
+				}
+			}
+		});
 	}
 }
