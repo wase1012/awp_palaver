bbce479b8d5f85679a4c99df7b55c23b7a8aac24
Christine Hartkorn <hach1014@hs-karlsruhe.de>
2013-04-29 10:41:58 +0200

    Kommentare für MenueplanGridLayout

--- palaver2/src/main/java/de/bistrosoft/palaver/menueplanverwaltung/MenueplanGridLayout.java
+++ palaver2/src/main/java/de/bistrosoft/palaver/menueplanverwaltung/MenueplanGridLayout.java
@@ -25,127 +25,129 @@ import fi.jasoft.dragdroplayouts.interfaces.DragFilter;
 @SuppressWarnings("serial")
 public class MenueplanGridLayout extends CustomComponent{
 
-    private static final int ROWS = 8;
+    // Konstanten
+	private static final int ROWS = 8;
     private static final int COLUMNS = 6;
        
+    // Seitenlayout erstellen
     public MenueplanGridLayout(int week, int year) {
-    setCaption("Kalenderwoche: " + week +"/"+year);
-    setSizeFull();
-
-    VerticalLayout outer = new VerticalLayout();
-    outer.setSizeFull();
-//    Label lbl = new Label ("sfd");
-//    outer.addComponent(lbl);
-    setCompositionRoot(outer);
-
-    // Create a drag and droppable grid layout
-    final DDGridLayout layout = new DDGridLayout(COLUMNS, ROWS);
-    int width = COLUMNS*150; 
-    int height = ROWS*100;
-    layout.setWidth(width+"px");
-    layout.setHeight(height+"px");
-   
-    // Only allow dropping in the center of the grid layout cell
-    layout.setComponentHorizontalDropRatio(0);
-    layout.setComponentVerticalDropRatio(0);
-
-    outer.addComponent(layout);
-    outer.setComponentAlignment(layout, Alignment.MIDDLE_CENTER);
-    outer.setExpandRatio(layout, 1);
-
-    // Enable dragging components
-    layout.setDragMode(LayoutDragMode.CLONE);
-
-    // Enable dropping components
-    layout.setDropHandler(new MenueplanGridDropHandler());
-    
-    // Limit dragging to only buttons
-    layout.setDragFilter(new DragFilter() {      
-    public boolean isDraggable(Component component) {  
-    	return component instanceof MenueComponent;
-        }
-            });
-    
-    //Fülle Überschriftenspalte
-    @SuppressWarnings("deprecation")
-	Label[] arlbUeb = {new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nDatum</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nKöche</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nHauptgericht 1</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nHauptgericht 2</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nVegetarisches Gericht</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nPastagericht</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nSalat / Suppe</font></pre>", Label.CONTENT_XHTML),
-    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nDessert</font></pre>", Label.CONTENT_XHTML)};
-    for (int i = 0; i < arlbUeb.length; i++) {
-    	arlbUeb[i].setWidth("150px");
-    	arlbUeb[i].setHeight("100px");
-    	layout.addComponent(arlbUeb[i],0,i);
-    	layout.setComponentAlignment(arlbUeb[i], Alignment.MIDDLE_CENTER);
-    }
-        
-    //Fülle Datumszeile
-
-	ArrayList<GregorianCalendar> dates = CalendarWeek.getDatesOfWeek(week, year);
-    for (int col = 1; col < COLUMNS; col++) {
-    	GregorianCalendar date = dates.get(col-1);
-    	String strDay = date.getDisplayName(Calendar.DAY_OF_WEEK, 2, Locale.GERMANY);
-
-    	String strDate = date.get(Calendar.DAY_OF_MONTH) + "." + 
-    						date.get(Calendar.MONTH) + "." + 
-    						date.get(Calendar.YEAR);
-    	
-    	Label lbTmp = new Label(strDay +"\r\n"+strDate);
-    	lbTmp.setWidth("140px");
-		layout.addComponent(lbTmp,col,0);	
-        layout.setComponentAlignment(lbTmp, Alignment.MIDDLE_CENTER);
-    }
-    
-    //Fülle Zeile für Köche
-    for (int col = 1; col < COLUMNS; col++) {
-    	VerticalLayout vl = new VerticalLayout();
-    	ComboBox koch1 = new ComboBox();
-        ComboBox koch2 = new ComboBox();
-    	koch1.setWidth("140px");
-    	koch1.addItem("Test");
-    	koch2.setWidth("140px");
-    	koch2.addItem("Test");
-    	vl.addComponent(koch1);
-    	vl.addComponent(koch2);
-    	layout.addComponent(vl,col,1);	
-        layout.setComponentAlignment(vl, Alignment.MIDDLE_CENTER);
-    }
-    
-    //Füge ADD Buttons ein
-    for (int row = 2; row < ROWS; row++) {
-        for (int col = 0; col < COLUMNS; col++) {
-        	if(layout.getComponent(col, row)==null) {
-        		Button btn = new Button("ADD");
-                btn.addClickListener(new ClickListener() {
-					
-					@Override
-					public void buttonClick(ClickEvent event) {
-						Button tmp = event.getButton();
-						for (int row = 0; row < ROWS; row++) {
-					        for (int col = 0; col < COLUMNS; col++) {
-					        	if(tmp.equals(layout.getComponent(col, row))) {
-					        		WinSelectMenue window = new WinSelectMenue(layout, tmp, row, col);
-					        		UI.getCurrent().addWindow(window);
-					        		window.setModal(true);
-					        		window.setWidth("50%");
-					        		window.setHeight("50%");
-					        		
-					        	}
-					        }
+	    setCaption("Kalenderwoche: " + week +"/"+year);
+	    setSizeFull();
+	
+	    // vertikales Layout anlegen
+	    VerticalLayout outer = new VerticalLayout();
+	    outer.setSizeFull();
+	    setCompositionRoot(outer);
+	
+	    // DragDropGridLayout erstellen
+	    final DDGridLayout layout = new DDGridLayout(COLUMNS, ROWS);
+	    int width = COLUMNS*150; 
+	    int height = ROWS*100;
+	    layout.setWidth(width+"px");
+	    layout.setHeight(height+"px");
+	   
+	    // Verschieben nur in die Zellenmitte erlauben
+	    layout.setComponentHorizontalDropRatio(0);
+	    layout.setComponentVerticalDropRatio(0);
+	
+	    // DragDropGridLayout zu Seitenlayout hinzufügen
+	    outer.addComponent(layout);
+	    outer.setComponentAlignment(layout, Alignment.MIDDLE_CENTER);
+	    outer.setExpandRatio(layout, 1);
+	
+	    // Wegschieben der DragDropGridLayout-Komponenten ermöglichen
+	    layout.setDragMode(LayoutDragMode.CLONE);
+	
+	    // Hinschieben der DragDropGridLayout-Komponenten ermöglichen
+	    layout.setDropHandler(new MenueplanGridDropHandler());
+	    
+	    // Limit dragging to only buttons
+	    layout.setDragFilter(new DragFilter() {      
+	    public boolean isDraggable(Component component) {  
+	    	return component instanceof MenueComponent;
+	        }
+	            });
+	    
+	    // Fülle Überschriftenspalte mit formatierten Labels
+	    @SuppressWarnings("deprecation")
+		Label[] arlbUeb = {new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nDatum</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nKöche</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nHauptgericht 1</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nHauptgericht 2</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nVegetarisches Gericht</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nPastagericht</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nSalat / Suppe</font></pre>", Label.CONTENT_XHTML),
+	    					new Label("<pre><font face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\"> \r \nDessert</font></pre>", Label.CONTENT_XHTML)};
+	    for (int i = 0; i < arlbUeb.length; i++) {
+	    	arlbUeb[i].setWidth("150px");
+	    	arlbUeb[i].setHeight("100px");
+	    	layout.addComponent(arlbUeb[i],0,i);
+	    	layout.setComponentAlignment(arlbUeb[i], Alignment.MIDDLE_CENTER);
+	    }
+	        
+	    // Fülle Datumszeile mit Wochentag und Datum
+		ArrayList<GregorianCalendar> dates = CalendarWeek.getDatesOfWeek(week, year);
+	    for (int col = 1; col < COLUMNS; col++) {
+	    	GregorianCalendar date = dates.get(col-1);
+	    	String strDay = date.getDisplayName(Calendar.DAY_OF_WEEK, 2, Locale.GERMANY);
+	
+	    	String strDate = date.get(Calendar.DAY_OF_MONTH) + "." + 
+	    						date.get(Calendar.MONTH) + "." + 
+	    						date.get(Calendar.YEAR);
+	    	
+	    	Label lbTmp = new Label(strDay +"\r\n"+strDate);
+	    	lbTmp.setWidth("140px");
+			layout.addComponent(lbTmp,col,0);	
+	        layout.setComponentAlignment(lbTmp, Alignment.MIDDLE_CENTER);
+	    }
+	    
+	    // Fülle Zeile für Köche mit DropDowm-Boxen für Namen
+	    for (int col = 1; col < COLUMNS; col++) {
+	    	VerticalLayout vl = new VerticalLayout();
+	    	ComboBox koch1 = new ComboBox();
+	        ComboBox koch2 = new ComboBox();
+	    	koch1.setWidth("140px");
+	    	koch1.addItem("Test");
+	    	koch2.setWidth("140px");
+	    	koch2.addItem("Test");
+	    	vl.addComponent(koch1);
+	    	vl.addComponent(koch2);
+	    	layout.addComponent(vl,col,1);	
+	        layout.setComponentAlignment(vl, Alignment.MIDDLE_CENTER);
+	    }
+	    
+	    // Füge ADD Buttons in noch leere Felder ein
+	    for (int row = 2; row < ROWS; row++) {
+	        for (int col = 0; col < COLUMNS; col++) {
+	        	if(layout.getComponent(col, row)==null) {
+	        		Button btn = new Button("ADD");
+	                btn.addClickListener(new ClickListener() {
+						
+	                	// Click-Listener für ADD_Buttons
+						@Override
+						public void buttonClick(ClickEvent event) {
+							Button tmp = event.getButton();
+							for (int row = 0; row < ROWS; row++) {
+						        for (int col = 0; col < COLUMNS; col++) {
+						        	if(tmp.equals(layout.getComponent(col, row))) {
+						        		WinSelectMenue window = new WinSelectMenue(layout, tmp, row, col);
+						        		UI.getCurrent().addWindow(window);
+						        		window.setModal(true);
+						        		window.setWidth("50%");
+						        		window.setHeight("50%");
+						        		
+						        	}
+						        }
+							}
 						}
-					}
-				});
-                
-                layout.addComponent(btn, col, row);
-                layout.setComponentAlignment(btn, Alignment.MIDDLE_CENTER);
-        	}
-                
-        }
-    }
-}
+					});
+	                
+	                layout.addComponent(btn, col, row);
+	                layout.setComponentAlignment(btn, Alignment.MIDDLE_CENTER);
+	        	}
+	                
+	        }
+	    }
+	}
 
 }
