790457b8ea7b2774580a821149e4f689108b2a09
bach1014 <barth_chris@hotmail.com>
2013-04-30 11:58:47 +0200

    update

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/BestellungTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/domain/BestellungTest.java
@@ -4,92 +4,107 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
+import java.sql.Timestamp;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
-
-import de.hska.awp.palaver2.bestellverwaltung.domain.Bestellung;
-import de.hska.awp.palaver2.data.ConnectException;
-import de.hska.awp.palaver2.data.DAOException;
-import de.hska.awp.palaver2.data.BestellungDAO;
-import de.hska.awp.palaver2.data.LieferantDAO;
-import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
-import de.hska.awp.palaver2.util.AbstractTest;
-
 
 import org.junit.Test;
 
 import de.hska.awp.palaver2.bestellverwaltung.domain.Bestellung;
-
+import de.hska.awp.palaver2.data.BestellungDAO;
+import de.hska.awp.palaver2.data.ConnectException;
+import de.hska.awp.palaver2.data.DAOException;
+import de.hska.awp.palaver2.data.LieferantDAO;
+import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
+import de.hska.awp.palaver2.util.AbstractTest;
 
 public class BestellungTest extends AbstractTest {
-	
+
 	final Long ID = Long.valueOf("2");
 	private BestellungDAO bdao = new BestellungDAO();
 	private LieferantDAO ldao = new LieferantDAO();
-	Date datum = new Date();
-	
-	 /**
+
+
+	/**
 	 * Die Testmethode erzeugt eine BEstellung in der Datenbank.
-     * @throws SQLException 
-     * @throws DAOException 
-     * @throws ConnectException 
+	 * 
+	 * @throws SQLException
+	 * @throws DAOException
+	 * @throws ConnectException
+	 * @throws ParseException
 	 */
-    @Test
-    public void createNewBestellung() throws ConnectException, DAOException, SQLException {
-    	
-    	Bestellung bestellung = new Bestellung();
-    	
-    	Long lid = Long.valueOf(1);
-    	Lieferant lieferant = null;
-    	lieferant = ldao.getLieferantById(lid);
-    	bestellung.setLieferant(lieferant);
-    	
-    	bestellung.setDatum(datum);
-    	
-    	bdao.createNewBestellung(bestellung);
-    	
-    }
+	@Test
+	public void createBestellung() throws ConnectException, DAOException,
+			SQLException, ParseException {
+
+		Bestellung bestellung = new Bestellung();
+
+		Long lid = Long.valueOf(1);
+		Lieferant lieferant = null;
+		lieferant = ldao.getLieferantById(lid);
+		bestellung.setLieferant(lieferant);
+
+		Date date = new Date();
+		Timestamp timestamp = new Timestamp(date.getTime());
+
+		bestellung.setDatum(timestamp);
+
+		bdao.createBestellung(bestellung);
+
+	}
 	
+	@Test
+	public void updateBestellung() throws ConnectException, DAOException,
+			SQLException, ParseException {
+
+		Bestellung bestellung = bdao.getBestellungById(ID);
+
+		Date date = new Date();
+		Timestamp timestamp = new Timestamp(date.getTime());
+
+		bestellung.setDatum(timestamp);
+
+		bdao.updateBestellung(bestellung);
+
+	}
 	
+
 	/**
 	 * Die Testmethode liefert alle Bestellungen aus der Datenbank zurück.
-	 * @throws SQLException 
-	 * @throws DAOException 
+	 * 
+	 * @throws SQLException
+	 * @throws DAOException
 	 * @throws ConnectException
 	 */
 	@Test
-	public void getBestellungen()
-	{
+	public void getBestellungen() {
 		Boolean exception = false;
 		List<Bestellung> bestellunglist = null;
-		try
-		{
+		try {
 			bestellunglist = bdao.getAllBestellungen();
-		} 
-		catch (ConnectException | DAOException | SQLException e)
-		{
+		} catch (ConnectException | DAOException | SQLException e) {
 			exception = true;
 		}
 		assertThat(bestellunglist.isEmpty(), is(false));
 		assertThat(exception, is(false));
 	}
-	
-	
+
 	/**
 	 * Die Testmethode sucht nach einer Bestellung anhand einer Id
- 	 * @throws SQLException 
- 	 * @throws DAOException 
- 	 * @throws ConnectException 
+	 * 
+	 * @throws SQLException
+	 * @throws DAOException
+	 * @throws ConnectException
 	 */
-	
-    @Test
-    public void findBestellungById() throws ConnectException, DAOException, SQLException {
 
-    	Bestellung bestellung = bdao.findBestellungById(ID);
+	@Test
+	public void getBestellungById() throws ConnectException, DAOException,
+			SQLException {
+
+		Bestellung bestellung = bdao.getBestellungById(ID);
 
 		assertThat(bestellung.getId(), is(ID));
-    }
-    
+	}
 
 }
