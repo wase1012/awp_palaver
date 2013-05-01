8e634993cfc38ae537354c7be8e3dbd4c27ba2f1
tuph1011 <tuph1011@hs-karlsruhe.de>
2013-05-01 22:49:54 +0200

    Umgeschrieben kein browser window mehr nur so n "popup" window, es funktioniert

--- palaver2/src/main/java/de/hska/awp/palaver2/gui/view/LieferantSuche.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/gui/view/LieferantSuche.java
@@ -1,7 +1,10 @@
 package de.hska.awp.palaver2.gui.view;
 
 import java.sql.SQLException;
+import java.util.List;
 
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.BeanItemContainer;
 import com.vaadin.server.BrowserWindowOpener;
 import com.vaadin.server.ThemeResource;
@@ -9,13 +12,18 @@ import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.ListSelect;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 
+import de.hska.awp.palaver2.artikelverwaltung.domain.Kategorie;
 import de.hska.awp.palaver2.artikelverwaltung.domain.Mengeneinheit;
+import de.hska.awp.palaver2.artikelverwaltung.service.Artikelverwaltung;
 import de.hska.awp.palaver2.artikelverwaltung.service.Mengeneinheitverwaltung;
 import de.hska.awp.palaver2.data.ConnectException;
 import de.hska.awp.palaver2.data.DAOException;
@@ -45,6 +53,11 @@ public class LieferantSuche extends VerticalLayout{
 	private Button 				ansprAdd = new Button(IConstants.BUTTON_ADD);
 	private Table 				ansprechpartner = new Table();
 	
+	private String			nameInput;
+	private String			telefonInput;
+	private String 			handyInput;
+	private String 			faxInput;
+	private Lieferant 		lieferant;
 
 	
 	public LieferantSuche() throws ConnectException, DAOException, SQLException {
@@ -94,7 +107,6 @@ public class LieferantSuche extends VerticalLayout{
 		
 		this.addComponent(box);
 		this.setComponentAlignment(box, Alignment.MIDDLE_CENTER);
-		Lieferant lieferant;
 
 		try {
 			lieferant = Lieferantenverwaltung.getInstance().getLastLieferant();
@@ -138,7 +150,7 @@ public class LieferantSuche extends VerticalLayout{
 			try {
 				container = new BeanItemContainer<Ansprechpartner>(Ansprechpartner.class, Ansprechpartnerverwaltung.getInstance().getAnsprechpartnerByLieferant(lieferant));
 				ansprechpartner.setContainerDataSource(container);
-				ansprechpartner.setVisibleColumns(new Object[] {"name"});
+				ansprechpartner.setVisibleColumns(new Object[] {"name", "telefon"});
 				ansprechpartner.sort(new Object[] {"id"}, new boolean[] {true});
 			} catch (IllegalArgumentException e) {
 				// TODO Auto-generated catch block
@@ -147,17 +159,141 @@ public class LieferantSuche extends VerticalLayout{
 		rechts.addComponent(ansprechpartner);	
 		
 		ansprAdd.addClickListener(new ClickListener() {
+		
+		@Override
+		public void buttonClick(ClickEvent event) {
+			final Window anspr = new Window();
+			anspr.setClosable(false);
+			anspr.setWidth("400px");
+			anspr.setHeight("400px");
+			anspr.setModal(true);
+			anspr.center();
+			anspr.setResizable(false);
+			anspr.setStyleName("dialog-window");
 			
-			@Override
-			public void buttonClick(ClickEvent event) {
-				BrowserWindowOpener opener = new BrowserWindowOpener(AnsprechpartnerAdd.class);
-					opener.setFeatures("height=400,width=600");
-					 
-					// Attach it to a button
-					opener.extend(ansprAdd);
-				
-			}
-		});
-	}
+			UI.getCurrent().addWindow(anspr);
+			
+			Label message = new Label("Ansprechpartner hinzufügen");
+			
+			VerticalLayout	layout = new VerticalLayout();
+			
+			TextField		nameAnspr = new TextField("Name");
+			TextField		telefon = new TextField("Telefon");
+			TextField		handy = new TextField("Handy");
+			TextField		fax = new TextField("Fax");
 
+			Button			speichern = new Button(IConstants.BUTTON_SAVE);
+			Button			verwerfen = new Button(IConstants.BUTTON_DISCARD);
+			
+			nameAnspr.setWidth("100%");
+			telefon.setWidth("100%");
+			handy.setWidth("100%");
+			fax.setWidth("100%");
+			
+			VerticalLayout feld = new VerticalLayout();
+			
+			box.setSpacing(true);
+		
+			feld.addComponent(nameAnspr);
+			feld.addComponent(telefon);
+			feld.addComponent(handy);
+			feld.addComponent(fax);
+
+			box.addComponentAsFirst(feld);
+			box.setComponentAlignment(feld, Alignment.MIDDLE_CENTER);
+
+			HorizontalLayout control = new HorizontalLayout();
+			control.setSpacing(true);
+			box.setWidth("300px");
+			box.addComponent(control);
+			box.setComponentAlignment(control, Alignment.BOTTOM_RIGHT);	
+			control.addComponent(verwerfen);
+			control.addComponent(speichern);
+			speichern.setIcon(new ThemeResource(IConstants.BUTTON_SAVE_ICON));
+			verwerfen.setIcon(new ThemeResource(IConstants.BUTTON_DISCARD_ICON));
+			
+			layout.addComponent(message);
+			layout.addComponent(feld);
+			layout.addComponent(control);
+			anspr.setContent(layout);
+			
+			nameAnspr.setImmediate(true);
+			nameAnspr.setInputPrompt(nameInput);
+			nameAnspr.setMaxLength(15);
+			
+			telefon.setImmediate(true);
+			telefon.setInputPrompt(telefonInput);
+			telefon.setMaxLength(10);	
+			
+			handy.setImmediate(true);
+			handy.setInputPrompt(handyInput);
+			handy.setMaxLength(10);
+			
+			fax.setImmediate(true);
+			fax.setInputPrompt(faxInput);
+			fax.setMaxLength(10);
+			  
+			speichern.addClickListener(new ClickListener()
+			{
+				public void buttonClick(ClickEvent event)
+				{
+					Ansprechpartner ans = new Ansprechpartner();
+					ans.setName(nameInput);
+					ans.setTelefon(telefonInput);
+					ans.setHandy(handyInput);
+					ans.setFax(faxInput);
+					ans.setLieferant(lieferant);
+					try {
+						Ansprechpartnerverwaltung.getInstance().createAnsprechpartner(ans);
+					} catch (ConnectException | DAOException | SQLException e) {
+						throw new NullPointerException("Bitte gültige Werte eingeben");
+					}				
+					
+					UI.getCurrent().removeWindow(anspr);
+					ViewHandler.getInstance().switchView(LieferantSuche.class);
+				}
+			});
+
+	        nameAnspr.addValueChangeListener(new ValueChangeListener() {
+
+	            public void valueChange(final ValueChangeEvent event) {
+	                final String valueString = String.valueOf(event.getProperty()
+	                        .getValue());
+
+	                nameInput = valueString;
+	            }
+	        });
+	        
+	        telefon.addValueChangeListener(new ValueChangeListener() {
+	            @Override
+	            public void valueChange(final ValueChangeEvent event) {
+	                final String valueString = String.valueOf(event.getProperty()
+	                        .getValue());
+	                telefonInput = valueString;
+	            }
+	        });
+	        
+	        handy.addValueChangeListener(new ValueChangeListener() {
+
+	            public void valueChange(final ValueChangeEvent event) {
+	                final String valueString = String.valueOf(event.getProperty()
+	                        .getValue());
+
+	                handyInput = valueString;
+	            }
+	        });
+	        
+	        fax.addValueChangeListener(new ValueChangeListener() {
+
+	            public void valueChange(final ValueChangeEvent event) {
+	                final String valueString = String.valueOf(event.getProperty()
+	                        .getValue());
+
+	                faxInput = valueString;
+	            }
+	        });
+		}
+	});
+
+	}
 }
