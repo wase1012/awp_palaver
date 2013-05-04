eb3953f15707218732f701cf5a28f3c2c5cdfcf6
bomi1012 <bomi1012@hs-karlsruhe.de>
2013-05-04 09:02:30 +0200

    KategorieErstellen KategorienAnzeigen

---
+++ palaver2/src/main/java/de/hska/awp/palaver2/gui/view/KategorieErstellen.java
@@ -0,0 +1,110 @@
+package de.hska.awp.palaver2.gui.view;
+
+import java.sql.SQLException;
+
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.data.validator.StringLengthValidator;
+import com.vaadin.server.ThemeResource;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
+
+import de.hska.awp.palaver2.artikelverwaltung.domain.Kategorie;
+import de.hska.awp.palaver2.artikelverwaltung.service.Kategorienverwaltung;
+import de.hska.awp.palaver2.data.ConnectException;
+import de.hska.awp.palaver2.data.DAOException;
+import de.hska.awp.palaver2.util.IConstants;
+import de.hska.awp.palaver2.util.View;
+import de.hska.awp.palaver2.util.ViewData;
+import de.hska.awp.palaver2.util.ViewHandler;
+
+/**
+ * 
+ * @author Mihail Boehm
+ *
+ */
+@SuppressWarnings("serial")
+public class KategorieErstellen extends VerticalLayout  implements View{
+
+private VerticalLayout	box = new VerticalLayout();
+	
+	private TextField		name = new TextField("Name");
+	private String			nameText;
+	
+	private Button			speichern = new Button(IConstants.BUTTON_SAVE);
+	private Button			verwerfen = new Button(IConstants.BUTTON_DISCARD);
+	
+	
+	
+	public KategorieErstellen() {
+		super();
+		this.setSizeFull();
+		this.setMargin(true);
+		
+		name.setWidth("100%");	
+		box.setSpacing(true);
+
+		this.addComponent(box);
+		this.setComponentAlignment(box, Alignment.MIDDLE_CENTER);		
+		box.addComponent(name);
+	
+		HorizontalLayout control = new HorizontalLayout();
+		control.setSpacing(true);
+		box.setWidth("300px");
+		box.addComponent(control);
+		box.setComponentAlignment(control, Alignment.MIDDLE_RIGHT);	
+		control.addComponent(verwerfen);
+		control.addComponent(speichern);
+		speichern.setIcon(new ThemeResource(IConstants.BUTTON_SAVE_ICON));
+		verwerfen.setIcon(new ThemeResource(IConstants.BUTTON_DISCARD_ICON));
+		
+		name.setImmediate(true);
+		name.setMaxLength(15);
+		name.addValidator(new StringLengthValidator("Bitte gültigen Namen eingeben", 4,15, false));
+		
+		verwerfen.addClickListener(new ClickListener() {
+			@Override
+			public void buttonClick(ClickEvent event) {
+				ViewHandler.getInstance().switchView(KategorienAnzeigen.class);							
+			}
+		});
+
+		speichern.addClickListener(new ClickListener()
+		{
+			public void buttonClick(ClickEvent event)
+			{
+				Kategorie ka = new Kategorie();
+				ka.setName(nameText);
+				try {
+					Kategorienverwaltung.getInstance().createNewKategorie(ka);
+				} catch (ConnectException | DAOException | SQLException e) {
+					throw new NullPointerException("Bitte gültige Werte eingeben");
+				}
+				ViewHandler.getInstance().switchView(KategorienAnzeigen.class);
+			}
+		});
+
+
+        name.addValueChangeListener(new ValueChangeListener() {
+
+            public void valueChange(final ValueChangeEvent event) {
+                final String valueString = String.valueOf(event.getProperty()
+                        .getValue());
+
+                nameText = valueString;
+            }
+        });
+        }
+
+	@Override
+	public void getViewParam(ViewData data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
