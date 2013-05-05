a1f8dad233ba26ee834b973e1d707d49ad77b654
wase1012 <wase1012@hs-karlsruhe.de>
2013-05-05 15:56:39 +0200

    ArtikelDAO schneller

--- palaver2/src/main/java/de/hska/awp/palaver2/data/ArtikelDAO.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/data/ArtikelDAO.java
@@ -13,6 +13,7 @@ import com.vaadin.data.util.converter.ConverterUtil;
 
 import de.hska.awp.palaver2.artikelverwaltung.domain.Artikel;
 import de.hska.awp.palaver2.artikelverwaltung.domain.Mengeneinheit;
+import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
 import de.hska.awp.palaver2.util.Util;
 
 public class ArtikelDAO extends AbstractDAO
@@ -25,6 +26,7 @@ public class ArtikelDAO extends AbstractDAO
 	private final static String			UPDATE_ARTIKEL = "UPDATE artikel SET `artikelnr` = {0},`name` = {1},`bestellgroesse` = {2},`mengeneinheit_fk` = {3},`preis` = {4},`lieferant_fk` = {5},`bio` = {6},`kategorie_fk` = {7},`standard` = {8},`grundbedarf` = {9},`durchschnitt` = {10},`lebensmittel` = {11} WHERE id = {12}";
 	private final static String			GET_ARTIKEL_BY_GRUNDBEDARF = "SELECT * FROM artikel WHERE grundbedarf=1";
 	private final static String			GET_ARTIKEL_BY_STANDARDBEDARF = "SELECT * FROM artikel WHERE standard=1";
+	private static final String 		GET_LIEFERANT_BY_ID = "SELECT * FROM lieferant WHERE id = {0}";
 	
 	public ArtikelDAO()
 	{
@@ -42,14 +44,16 @@ public class ArtikelDAO extends AbstractDAO
 	{
 		List<Artikel> list = new ArrayList<Artikel>();
 		
-		ResultSet set = getManaged(GET_ALL_ARTIKLES);
+		openConnection();
+		
+		ResultSet set = get(GET_ALL_ARTIKLES);
 		
 		while(set.next())
 		{
 			list.add(new Artikel(set.getLong("id"),
 								new Mengeneinheit(),
 								KategorieDAO.getInstance().getKategorieById(set.getLong("kategorie_fk")),
-								LieferantDAO.getInstance().getLieferantById(set.getLong("lieferant_fk")),
+								getLieferantById(set.getLong("lieferant_fk")),
 								set.getString("artikelnr"),
 								set.getString("name"),
 								set.getDouble("bestellgroesse"),
@@ -62,6 +66,7 @@ public class ArtikelDAO extends AbstractDAO
 								));
 		}
 		
+		closeConnection();
 		return list;
 	}
 	
@@ -236,5 +241,22 @@ public class ArtikelDAO extends AbstractDAO
 								Util.convertBoolean(artikel.isLebensmittel()),
 								artikel.getId()));
 								*/
+	}
+	
+	private Lieferant getLieferantById(Long id) throws SQLException
+	{
+		Lieferant lieferant = null;
+		ResultSet set = get(MessageFormat.format(GET_LIEFERANT_BY_ID, id));
+
+		while (set.next()) 
+		{
+			lieferant = new Lieferant(set.getLong("id"), set.getString("name"),
+			set.getString("kundennummer"), set.getString("bezeichnung"),
+			set.getString("strasse"), set.getString("plz"),
+			set.getString("ort"), set.getString("email"),
+			set.getString("telefon"), set.getString("fax"));
+		}
+
+			return lieferant;
 	}
 }
