f68172f96c45f8b8e59a6ffa89501347092843ba
Christine Hartkorn <hach1014@hs-karlsruhe.de>
2013-05-06 20:27:00 +0200

    MenueplanHistorie vervollständigt

--- palaver2/src/main/java/de/bistrosoft/palaver/gui/view/MenueplanHistorie.java
+++ palaver2/src/main/java/de/bistrosoft/palaver/gui/view/MenueplanHistorie.java
@@ -8,27 +8,24 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.Locale;
 
-import com.vaadin.client.metadata.Property;
 import com.vaadin.server.Page;
-import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.PopupDateField;
 import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.themes.BaseTheme;
 import com.vaadin.ui.Label;
 
 import de.bistrosoft.palaver.menueplanverwaltung.MenueplanGridLayout;
+import de.bistrosoft.palaver.menueplanverwaltung.domain.Menueplan;
+import de.bistrosoft.palaver.menueplanverwaltung.service.Menueplanverwaltung;
 import de.bistrosoft.palaver.util.CalendarWeek;
-import de.bistrosoft.palaver.util.ViewHandler;
 import de.bistrosoft.palaver.util.Week;
+import fi.jasoft.dragdroplayouts.client.ui.LayoutDragMode;
 
 
 @SuppressWarnings("serial")
@@ -64,45 +61,66 @@ public class MenueplanHistorie extends VerticalLayout{
 			// Click-Listener zur Datumsauswahl
 			@Override
 			public void buttonClick(ClickEvent event) {
-				//TODO: nur Vergangenheitswerte zulassen; Meldung, wenn nichts in DB 
+				//Datum in Woche und Jahr aufteilen
 				SimpleDateFormat sdf;
-		         Calendar cal;
-		         Date datum = null;
-		         int week;
-		         String sample = date.getValue().toString();
-		         sdf = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.ENGLISH);
-		         try {
+		        Calendar cal;
+		        Date datum = null;
+		        int week;
+		        String sample = date.getValue().toString();
+		        sdf = new SimpleDateFormat("EEE MMM d HH:mm:ss z yyyy", Locale.ENGLISH);
+		        try {
 					datum = sdf.parse(sample);
 				} catch (ParseException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		         cal = Calendar.getInstance();
-		         cal.setTime(datum);
-		         week = cal.get(Calendar.WEEK_OF_YEAR);
+		        cal = Calendar.getInstance();
+		        cal.setTime(datum);
+		        week = cal.get(Calendar.WEEK_OF_YEAR);
 				@SuppressWarnings("deprecation")
 				int year = date.getValue().getYear()+1900;
+				//alte Anzeigen löschen
 				if (Menueplan!=null){
 					box.removeComponent(Menueplan);
 					box.removeComponent(lbKW);
 					box.removeComponent(lbPlatzhalter1);
 					box.removeComponent(lbPlatzhalter2);
 				}
-				Menueplan = new MenueplanGridLayout(week, year);
-				String strKW= new String("Kalenderwoche: " + week +"/"+year);
-				@SuppressWarnings("deprecation")
-				Label lbKW2 = new Label("<pre><div align=center><font style=\"font-size: large\" face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\">"+strKW+"</div></pre>", Label.CONTENT_XHTML);
-				lbKW=lbKW2;
-				lbPlatzhalter1.setHeight("30px");
-		        box.addComponent(lbPlatzhalter1);
-		        box.setComponentAlignment(lbPlatzhalter1, Alignment.TOP_CENTER);
-				box.addComponent(lbKW);
-				box.setComponentAlignment(lbKW, Alignment.TOP_CENTER);
-				lbPlatzhalter2.setHeight("30px");
-		        box.addComponent(lbPlatzhalter2);
-		        box.setComponentAlignment(lbPlatzhalter2, Alignment.TOP_CENTER);
-				box.addComponent(Menueplan);
-				box.setComponentAlignment(Menueplan, Alignment.MIDDLE_CENTER);
+				//nur alte Pläne
+				Week curWeek = CalendarWeek.getCurrentWeek();
+				final int woche = curWeek.getWeek();
+				if (week<woche){
+					//nur gespeicherte Pläne
+					if (Menueplanverwaltung.getInstance().getMenueplanByWeek(new Week(week,year))==null){
+			        	Notification notification = new Notification("Kein Menüplan vorhanden.");
+						notification.setDelayMsec(500);
+						notification.show(Page.getCurrent());
+			    	}
+					else{
+						//Anzeige
+						Menueplan = new MenueplanGridLayout(week, year);
+						Menueplan.layout.setDragMode(LayoutDragMode.NONE);
+						String strKW= new String("Kalenderwoche: " + week +"/"+year);
+						@SuppressWarnings("deprecation")
+						Label lbKW2 = new Label("<pre><div align=center><font style=\"font-size: large\" face=\"Arial, Helvetica, Tahoma, Verdana, sans-serif\">"+strKW+"</div></pre>", Label.CONTENT_XHTML);
+						lbKW=lbKW2;
+						lbPlatzhalter1.setHeight("30px");
+				        box.addComponent(lbPlatzhalter1);
+				        box.setComponentAlignment(lbPlatzhalter1, Alignment.TOP_CENTER);
+						box.addComponent(lbKW);
+						box.setComponentAlignment(lbKW, Alignment.TOP_CENTER);
+						lbPlatzhalter2.setHeight("30px");
+				        box.addComponent(lbPlatzhalter2);
+				        box.setComponentAlignment(lbPlatzhalter2, Alignment.TOP_CENTER);
+				        box.addComponent(Menueplan);
+						box.setComponentAlignment(Menueplan, Alignment.MIDDLE_CENTER);
+					}
+				}
+				else{
+					Notification notification = new Notification("Menüplan muss in der Vergangenheit liegen.");
+					notification.setDelayMsec(500);
+					notification.show(Page.getCurrent());
+				}
 			}
 		});
 		Label lbPlatzhalter =new Label();
