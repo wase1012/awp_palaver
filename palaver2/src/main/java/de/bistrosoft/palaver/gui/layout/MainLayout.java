78dfb0dad4ef5751be6578d373e5e0b8d5b6cbc8
Michael Marschall <mami1017@hs-karlsruhe.de>
2013-05-08 10:50:22 +0200

    Reiter Einstellungen in MainLayout erweitert

--- palaver2/src/main/java/de/bistrosoft/palaver/gui/layout/MainLayout.java
+++ palaver2/src/main/java/de/bistrosoft/palaver/gui/layout/MainLayout.java
@@ -24,125 +24,135 @@ import de.bistrosoft.palaver.gui.view.RezeptAnlegen;
 import de.bistrosoft.palaver.gui.view.RezeptAnzeigen;
 import de.bistrosoft.palaver.gui.view.RezeptAnzeigenTabelle;
 import de.bistrosoft.palaver.gui.view.RezeptSuchen;
+import de.bistrosoft.palaver.gui.view.Zubereitungen;
 import de.bistrosoft.palaver.util.IConstants;
 import de.bistrosoft.palaver.util.ViewHandler;
 
 /**
  * @author Sebastian
- *
+ * 
  */
 @SuppressWarnings("serial")
-public class MainLayout extends VerticalLayout implements Command
-{
-	private static MainLayout		instance = null;
-	
-	private HorizontalLayout		header = new HorizontalLayout();
-	
-	private MenuBar					menu = new MenuBar();
-	
+public class MainLayout extends VerticalLayout implements Command {
+	private static MainLayout instance = null;
+
+	private HorizontalLayout header = new HorizontalLayout();
+
+	private MenuBar menu = new MenuBar();
+
 	@SuppressWarnings("deprecation")
-	private MainLayout()
-	{
+	private MainLayout() {
 		super();
-		
+
 		this.setSizeFull();
-		
+
 		header.setWidth("100%");
 		header.setHeight("100px");
 		header.setStyleName("palaver-header");
-		
-//		Image logo = new Image(null, new ThemeResource("../img/cafe_palaver_Logo.png"));
-//		header.addComponent(logo);
-//		header.setComponentAlignment(logo, Alignment.MIDDLE_RIGHT);
-//		logo.addClickListener(new ClickListener()
-//		{
-//			@Override
-//			public void click(ClickEvent event)
-//			{
-//				ViewHandler.getInstance().switchView(DefaultView.class);
-//			}
-//		});
-		
-		header.addListener(new LayoutClickListener()
-		{
+
+		// Image logo = new Image(null, new
+		// ThemeResource("../img/cafe_palaver_Logo.png"));
+		// header.addComponent(logo);
+		// header.setComponentAlignment(logo, Alignment.MIDDLE_RIGHT);
+		// logo.addClickListener(new ClickListener()
+		// {
+		// @Override
+		// public void click(ClickEvent event)
+		// {
+		// ViewHandler.getInstance().switchView(DefaultView.class);
+		// }
+		// });
+
+		header.addListener(new LayoutClickListener() {
 
 			@Override
-			public void layoutClick(LayoutClickEvent event)
-			{
+			public void layoutClick(LayoutClickEvent event) {
 				ViewHandler.getInstance().switchView(DefaultView.class);
 			}
 		});
-		
+
 		this.addComponent(header);
-		
+
 		menu.setWidth("100%");
-		MenuItem artikelItem = menu.addItem(IConstants.MENU_ARTIKEL_HEADLINE, null);
+		MenuItem artikelItem = menu.addItem(IConstants.MENU_ARTIKEL_HEADLINE,
+				null);
 		artikelItem.addItem(IConstants.MENU_ARTIKEL_NEU, this);
 		artikelItem.addItem(IConstants.MENU_ARTIKEL_ANZEIGEN, this);
-		
-		MenuItem lieferantItem = menu.addItem(IConstants.MENU_LIEFERANT_HEADLINE, null);
+
+		MenuItem lieferantItem = menu.addItem(
+				IConstants.MENU_LIEFERANT_HEADLINE, null);
 		lieferantItem.addItem(IConstants.MENU_LIEFERANT_NEW, this);
 		lieferantItem.addItem(IConstants.MENU_LIEFERANT_ANZEIGEN, this);
-		
-		MenuItem rezeptItem = menu.addItem(IConstants.MENU_REZEPT_HEADLINE, null);
+
+		MenuItem rezeptItem = menu.addItem(IConstants.MENU_REZEPT_HEADLINE,
+				null);
 		rezeptItem.addItem(IConstants.MENU_REZEPT_NEU, this);
 		rezeptItem.addItem(IConstants.MENU_REZEPT_ANZEIGEN, this);
-		
-		MenuItem menuplanItem = menu.addItem(IConstants.MENU_MENUPLAN_HEADLINE, null);
+
+		MenuItem menuplanItem = menu.addItem(IConstants.MENU_MENUPLAN_HEADLINE,
+				null);
 		menuplanItem.addItem(IConstants.MENU_MENUPLAN_AKTUELL, this);
 		menuplanItem.addItem(IConstants.MENU_MENUPLAN_HISTORIE, this);
-		
-		MenuItem bestellungItem = menu.addItem(IConstants.MENU_BESTELLUNG_HEADLINE, null);
-		
-		MenuItem einstellungItem = menu.addItem(IConstants.MENU_EINSTELLUNGEN_HEADLINE, null);
+
+		MenuItem bestellungItem = menu.addItem(
+				IConstants.MENU_BESTELLUNG_HEADLINE, null);
+
+		MenuItem einstellungItem = menu.addItem(
+				IConstants.MENU_EINSTELLUNGEN_HEADLINE, null);
+		einstellungItem.addItem(IConstants.MENU_FUSSNOTE, this);
+		einstellungItem.addItem(IConstants.MENU_GESCHMACK, this);
+		einstellungItem.addItem(IConstants.MENU_REZEPTART, this);
+		einstellungItem.addItem(IConstants.MENU_ZUBEREITUNG, this);
 		this.addComponent(menu);
-		
+
 		DefaultView content = new DefaultView();
 		this.addComponent(content);
 		this.setExpandRatio(content, 1);
-		
-//		this.addComponent(new ArtikelErstellen());
-//		this.setExpandRatio(this.getComponent(2), 1);
+
+		// this.addComponent(new ArtikelErstellen());
+		// this.setExpandRatio(this.getComponent(2), 1);
 	}
-	
-	public static MainLayout getInstance()
-	{
-		if (instance == null)
-		{
+
+	public static MainLayout getInstance() {
+		if (instance == null) {
 			instance = new MainLayout();
 		}
 		return instance;
 	}
 
 	@Override
-	  public void menuSelected(MenuItem selectedItem)
-	  {
-	    if (selectedItem.getText().equals(IConstants.MENU_ARTIKEL_NEU))
-	    {
-	      ViewHandler.getInstance().switchView(ArtikelErstellen.class);
-	    }
-	    if (selectedItem.getText().equals(IConstants.MENU_LIEFERANT_NEW))
-	    {
-	      ViewHandler.getInstance().switchView(LieferantErstellen.class);
-	    }
-	    if (selectedItem.getText().equals(IConstants.MENU_REZEPT_NEU))
-	    {
-	      ViewHandler.getInstance().switchView(RezeptAnlegen.class);
-	    }
-	    if (selectedItem.getText().equals(IConstants.MENU_REZEPT_ANZEIGEN))
-		    {
-		      ViewHandler.getInstance().switchView(RezeptAnzeigenTabelle.class);
-		    }
-	    if (selectedItem.getText().equals(IConstants.MENU_MENUPLAN_AKTUELL))
-	    {
-	      ViewHandler.getInstance().switchView(Menueplan.class);
-	    }
-	    if (selectedItem.getText().equals(IConstants.MENU_MENUPLAN_HISTORIE))
-	    {
-	      ViewHandler.getInstance().switchView(MenueplanHistorie.class);
-	    }
+	public void menuSelected(MenuItem selectedItem) {
+		if (selectedItem.getText().equals(IConstants.MENU_ARTIKEL_NEU)) {
+			ViewHandler.getInstance().switchView(ArtikelErstellen.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_LIEFERANT_NEW)) {
+			ViewHandler.getInstance().switchView(LieferantErstellen.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_REZEPT_NEU)) {
+			ViewHandler.getInstance().switchView(RezeptAnlegen.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_REZEPT_ANZEIGEN)) {
+			ViewHandler.getInstance().switchView(RezeptAnzeigenTabelle.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_MENUPLAN_AKTUELL)) {
+			ViewHandler.getInstance().switchView(Menueplan.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_MENUPLAN_HISTORIE)) {
+			ViewHandler.getInstance().switchView(MenueplanHistorie.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_FUSSNOTE)) {
+			ViewHandler.getInstance().switchView(MenueplanHistorie.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_GESCHMACK)) {
+			ViewHandler.getInstance().switchView(MenueplanHistorie.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_REZEPTART)) {
+			ViewHandler.getInstance().switchView(MenueplanHistorie.class);
+		}
+		if (selectedItem.getText().equals(IConstants.MENU_ZUBEREITUNG)) {
+			ViewHandler.getInstance().switchView(Zubereitungen.class);
+		}
 
-	  }
-	 
 	}
 
+}
--- palaver2/src/main/java/de/bistrosoft/palaver/util/IConstants.java
+++ palaver2/src/main/java/de/bistrosoft/palaver/util/IConstants.java
@@ -5,27 +5,30 @@
 
 package de.bistrosoft.palaver.util;
 
-public interface IConstants
-{
-	public final String		DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/palaver?user=root&password=awp";
-	
-	public final String		MENU_ARTIKEL_HEADLINE = "Artikel";
-	public final String		MENU_ARTIKEL_NEU = "Artikel anlegen";
-	public final String		MENU_ARTIKEL_ANZEIGEN = "Alle Artikel";
-	public final String		MENU_LIEFERANT_HEADLINE = "Lieferant";
-	public final String		MENU_LIEFERANT_NEW = "Lieferant anlegen";
-	public final String		MENU_LIEFERANT_ANZEIGEN = "Alle Lieferanten";
-	public final String		MENU_EINSTELLUNGEN_HEADLINE = "Einstellungen";
-	public final String		MENU_BESTELLUNG_HEADLINE = "Bestellung";
-	
-	public final String		MENU_REZEPT_HEADLINE = "Rezept";
-	public final String		MENU_REZEPT_NEU = "Anlegen";
-	public final String 	MENU_REZEPT_ANZEIGEN = "Anzeigen";
-	public final String 	MENU_REZEPT_AENDERN = "Ändern";
-	public final String 	MENU_REZEPT_LOESCHEN = "Löschen";
-	
-	public final String 	MENU_MENUPLAN_HEADLINE = "Menüplan";
-	public final String		MENU_MENUPLAN_AKTUELL = "Aktueller Menüplan";
-	public final String 	MENU_MENUPLAN_HISTORIE = "Menüplan-Historie";
-}
+public interface IConstants {
+	public final String DB_CONNECTION_URL = "jdbc:mysql://localhost:3306/palaver?user=root&password=awp";
 
+	public final String MENU_ARTIKEL_HEADLINE = "Artikel";
+	public final String MENU_ARTIKEL_NEU = "Artikel anlegen";
+	public final String MENU_ARTIKEL_ANZEIGEN = "Alle Artikel";
+	public final String MENU_LIEFERANT_HEADLINE = "Lieferant";
+	public final String MENU_LIEFERANT_NEW = "Lieferant anlegen";
+	public final String MENU_LIEFERANT_ANZEIGEN = "Alle Lieferanten";
+	public final String MENU_BESTELLUNG_HEADLINE = "Bestellung";
+
+	public final String MENU_REZEPT_HEADLINE = "Rezept";
+	public final String MENU_REZEPT_NEU = "Anlegen";
+	public final String MENU_REZEPT_ANZEIGEN = "Anzeigen";
+	public final String MENU_REZEPT_AENDERN = "Ändern";
+	public final String MENU_REZEPT_LOESCHEN = "Löschen";
+
+	public final String MENU_MENUPLAN_HEADLINE = "Menüplan";
+	public final String MENU_MENUPLAN_AKTUELL = "Aktueller Menüplan";
+	public final String MENU_MENUPLAN_HISTORIE = "Menüplan-Historie";
+
+	public final String MENU_EINSTELLUNGEN_HEADLINE = "Einstellungen";
+	public final String MENU_FUSSNOTE = "Fussnoten";
+	public final String MENU_GESCHMACK = "Geschmäcker";
+	public final String MENU_REZEPTART = "Rezeptarten";
+	public final String MENU_ZUBEREITUNG = "Zubereitungen";
+}
