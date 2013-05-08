53102860a281d5ebcad2e21bb127adffb4d9dd39
weel1012 <weel1011@hs-karlsruhe.de>
2013-05-08 12:30:12 +0200

    AV Test getbyId

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/ArtikelverwaltungTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/ArtikelverwaltungTest.java
@@ -4,7 +4,43 @@
  */
 package de.hska.awp.palaver2.testklassen.service;
 
-public class ArtikelverwaltungTest
-{
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import de.hska.awp.palaver2.artikelverwaltung.domain.Artikel;
+import de.hska.awp.palaver2.artikelverwaltung.service.Artikelverwaltung;
+import de.hska.awp.palaver2.data.ConnectException;
+import de.hska.awp.palaver2.data.DAOException;
+
+
+public class ArtikelverwaltungTest {
+	
+Artikelverwaltung av = Artikelverwaltung.getInstance();		 
+private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(1);	
+
+
+	@Test
+	public void getArtikelById(){
+		
+	
+		Artikel art = null;
+		Boolean exception = false;
+		final Long id = ARTIKEL_ID_VORHANDEN;
+		
+		try{
+		 art = av.getArtikelById(id);
+		}
+		catch (ConnectException | DAOException | SQLException e)
+		{
+			exception = true;
+		}
+		assertThat(art.getId(),is(id));
+	}
 }
+
