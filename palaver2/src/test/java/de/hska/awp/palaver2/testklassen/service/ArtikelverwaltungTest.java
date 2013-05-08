574ebdce1d614590bc30492f646b7615bf966e37
weel1012 <weel1011@hs-karlsruhe.de>
2013-05-08 13:08:49 +0200

    AV Test Create&Update, getByName

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/ArtikelverwaltungTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/ArtikelverwaltungTest.java
@@ -14,15 +14,25 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 import de.hska.awp.palaver2.artikelverwaltung.domain.Artikel;
+import de.hska.awp.palaver2.artikelverwaltung.domain.Kategorie;
+import de.hska.awp.palaver2.artikelverwaltung.domain.Mengeneinheit;
 import de.hska.awp.palaver2.artikelverwaltung.service.Artikelverwaltung;
 import de.hska.awp.palaver2.data.ConnectException;
 import de.hska.awp.palaver2.data.DAOException;
+import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
 
 
 public class ArtikelverwaltungTest {
 	
 Artikelverwaltung av = Artikelverwaltung.getInstance();		 
-private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(1);	
+private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(1);
+final Mengeneinheit menge = new Mengeneinheit(); 
+final Kategorie kategorie = new Kategorie();
+final Lieferant lieferant= new Lieferant();
+final Boolean bio = true;
+final Boolean standard = true;
+final Boolean grundbedarf = true;
+final Boolean lebensmittel = true;
 
 
 	@Test
@@ -33,6 +43,7 @@ private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(1);
 		Boolean exception = false;
 		final Long id = ARTIKEL_ID_VORHANDEN;
 		
+		
 		try{
 		 art = av.getArtikelById(id);
 		}
@@ -41,6 +52,92 @@ private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(1);
 			exception = true;
 		}
 		assertThat(art.getId(),is(id));
+	}
+	
+	@Test
+    public void getArtikelByName() {
+    	
+    	final String name = "Walnusskerne 500G";
+    	Boolean exception = false;
+    	List<Artikel> artikellist = null;
+	
+    		try{
+    			
+    			artikellist = av.getArtikelByName(name);
+    		
+    		}
+    		catch (ConnectException | DAOException | SQLException e)
+    		{
+    			exception = true;
+    		}
+    		for (Artikel a : artikellist) {
+    			assertThat(a.getName(), is(name));
+    		}
+    	}
+	@Test
+	public void createArtikel(){
+		
+		Artikel art = new Artikel();
+		Boolean exception = false;
+		
+		final String name = "SuperEis";
+		
+		
+		art.setName(name);
+		art.setMengeneinheit(menge);
+	    art.setKategorie(kategorie);
+		art.setLieferant(lieferant);
+		art.setBio(bio);
+		art.setStandard(standard);
+		art.setGrundbedarf(grundbedarf);
+		art.setLebensmittel(lebensmittel);
+		
+		try{
+	    av.createArtikel(art);
+		}
+		catch (ConnectException | DAOException e)
+		{
+			exception = true;
+		}
+		
+		assertThat(art.getName(),is(name));
+	}
+	
+	@Test
+	public void updateArtikel (){
+		
+		Artikel art = new Artikel();
+		
+		Boolean exception = false;
+		final Long id = Long.valueOf(1);
+		final String neuerName = "TestFleisch";
+		
+		try{
+			
+		av.getArtikelById(id);
+		
+		}
+		catch (ConnectException | DAOException | SQLException e)
+		{
+			exception = true;
+		}
+		art.setName(neuerName);
+		art.setMengeneinheit(menge);
+	    art.setKategorie(kategorie);
+		art.setLieferant(lieferant);
+		art.setBio(bio);
+		art.setStandard(standard);
+		art.setGrundbedarf(grundbedarf);
+		art.setLebensmittel(lebensmittel);
+		
+		try{
+		av.updateArtikel(art);
+		}
+		catch (ConnectException | DAOException e)
+		{
+			exception = true;
+		}
+		assertThat(art.getName(), is(neuerName));
 	}
 }
 
