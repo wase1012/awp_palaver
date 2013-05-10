9e1df7907b093bdd213f44f115b1f78b9ed22011
weel1012 <weel1011@hs-karlsruhe.de>
2013-05-10 21:44:37 +0200

    BV Test Create & Update funktionieren

--- palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/BestellverwaltungTest.java
+++ palaver2/src/test/java/de/hska/awp/palaver2/testklassen/service/BestellverwaltungTest.java
@@ -8,11 +8,13 @@ import static org.junit.Assert.assertThat;
 
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.*;
 
 import org.junit.Test;
 
-import de.hska.awp.palaver2.artikelverwaltung.domain.Kategorie;
+import de.hska.awp.palaver2.bestellverwaltung.domain.Bestellposition;
 import de.hska.awp.palaver2.bestellverwaltung.domain.Bestellung;
 import de.hska.awp.palaver2.bestellverwaltung.service.Bestellverwaltung;
 import de.hska.awp.palaver2.data.ConnectException;
@@ -28,7 +30,7 @@ public class BestellverwaltungTest {
 	Bestellverwaltung bv = Bestellverwaltung.getInstance();
 	private static final Long BESTELLUNG_ID_VORHANDEN = Long.valueOf(1);
 	final Lieferant lieferant = new Lieferant();
-	final java.sql.Date datum = (java.sql.Date) new Date();
+	final java.sql.Date datum = (java.sql.Date) new Date(0);
 	
 	/**
 	 * Testmethode createBestellung
@@ -46,6 +48,9 @@ public class BestellverwaltungTest {
 				
 		b.setDatum(datum);
 		b.setLieferant(lieferant);
+		List<Bestellposition> bestellpositionen = new ArrayList<Bestellposition>();
+		 
+		b.setBestellpositionen(bestellpositionen);
 				
 		try{
 	    bv.createBestellung(b);
@@ -56,6 +61,45 @@ public class BestellverwaltungTest {
 		}
 		
 		assertThat(b.getDatum(),is(datum));
+	}
+	
+	 /**
+	 * Testmethode updateBEstellung
+	 * Aktualiesiert eine Bestellung in der Datenbank
+     * @throws SQLException 
+     * @throws DAOException 
+     * @throws ConnectException 
+	 */
+@Test
+	public void updateBestellung (){
+	
+		Bestellung b = new Bestellung();
+	
+		Boolean exception = false;
+		final Long id = Long.valueOf(1);
+		
+	
+		try{
+		
+			bv.getBestellungById(id);
+	
+		}
+		catch (ConnectException | DAOException | SQLException e)
+		{
+			exception = true;
+		}
+		
+		b.setLieferant(lieferant);
+		b.setDatum(datum);
+	
+		try{
+			bv.updateBestellung(b);
+		}
+		catch (ConnectException | DAOException| SQLException e)
+		{
+			exception = true;
+		}
+		assertThat(b.getDatum(), is(datum));
 	}
 
 }
