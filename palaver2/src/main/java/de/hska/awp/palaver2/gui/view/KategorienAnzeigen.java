4d50560d476ba4bab00ec33fbb5d39ed426fa8cc
bach1014 <barth_chris@hotmail.com>
2013-05-12 10:33:09 +0200

    Updatefunktion in die GUI eingebaut für Kategorie

--- palaver2/src/main/java/de/hska/awp/palaver2/gui/view/KategorienAnzeigen.java
+++ palaver2/src/main/java/de/hska/awp/palaver2/gui/view/KategorienAnzeigen.java
@@ -2,14 +2,23 @@ package de.hska.awp.palaver2.gui.view;
 
 import java.sql.SQLException;
 
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.validator.StringLengthValidator;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.Table;
-import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 import de.hska.awp.palaver2.artikelverwaltung.domain.Kategorie;
 import de.hska.awp.palaver2.artikelverwaltung.service.Kategorienverwaltung;
@@ -31,6 +40,12 @@ public class KategorienAnzeigen  extends VerticalLayout  implements View{
 	private Button hinzufuegen = new Button(IConstants.BUTTON_ADD);
 	private Table table;
 	
+	private TextField			name = new TextField("Name");
+	private TextField			nameUp = new TextField("Name");
+	private String				nameText;
+	
+	private Kategorie		kategorieUpdate;
+	
 	public KategorienAnzeigen(){
 		super();
 		
@@ -41,6 +56,110 @@ public class KategorienAnzeigen  extends VerticalLayout  implements View{
 		layout.setSpacing(true);
 		table = new Table();
 		table.setSizeFull();
+		table.setSelectable(true);
+		
+		table.addValueChangeListener(new ValueChangeListener() {
+
+			@Override
+			public void valueChange(ValueChangeEvent event) {
+				if (event.getProperty().getValue() != null) {
+					kategorieUpdate = (Kategorie) event.getProperty()
+							.getValue();
+				}
+			}
+		});
+		
+		table.addItemClickListener(new ItemClickListener() {	
+			@Override
+			public void itemClick(ItemClickEvent event) {
+				if(event.isDoubleClick()){
+					final Window mengNeu = new Window();
+					mengNeu.setClosable(false);
+					mengNeu.setWidth("400px");
+					mengNeu.setHeight("270px");
+					mengNeu.setModal(true);
+					mengNeu.center();
+					mengNeu.setResizable(false);
+					mengNeu.setCaption("Kategorie hinzufügen");
+					
+					UI.getCurrent().addWindow(mengNeu);
+					
+					VerticalLayout	layout = new VerticalLayout();
+					layout.setMargin(true);
+					layout.setWidth("100%");
+					layout.setSpacing(true);
+
+					Button			speichern = new Button(IConstants.BUTTON_SAVE);
+					Button			verwerfen = new Button(IConstants.BUTTON_DISCARD);
+					
+					nameUp.setWidth("100%");
+
+					VerticalLayout feld = new VerticalLayout();
+				
+					feld.addComponent(nameUp);
+
+					HorizontalLayout control = new HorizontalLayout();
+					control.setSpacing(true);
+					control.addComponent(verwerfen);
+					control.addComponent(speichern);
+					speichern.setIcon(new ThemeResource(IConstants.BUTTON_SAVE_ICON));
+					verwerfen.setIcon(new ThemeResource(IConstants.BUTTON_DISCARD_ICON));
+
+					layout.addComponent(feld);
+					layout.setComponentAlignment(feld, Alignment.MIDDLE_CENTER);
+					layout.addComponent(control);
+					layout.setComponentAlignment(control, Alignment.BOTTOM_RIGHT);
+					mengNeu.setContent(layout);
+					
+					nameUp.setImmediate(true);
+					nameUp.setValue(kategorieUpdate.getName());
+					nameUp.setMaxLength(45);
+					nameUp.addValidator(new StringLengthValidator("Bitte gültigen Namen eingeben", 4,45, false));
+					
+					verwerfen.addClickListener(new ClickListener() {
+						
+						@Override
+						public void buttonClick(ClickEvent event) {
+							UI.getCurrent().removeWindow(mengNeu);
+							ViewHandler.getInstance().switchView(KategorienAnzeigen.class);							
+						}
+					});
+
+					speichern.addClickListener(new ClickListener()
+					{
+						public void buttonClick(ClickEvent event)
+						{
+							kategorieUpdate.setName(nameUp.getValue());
+							try {
+								Kategorienverwaltung.getInstance().updateKategorie(kategorieUpdate);
+							} catch (ConnectException | DAOException | SQLException e) {
+								throw new NullPointerException("Bitte gültige Werte eingeben");
+							}
+							UI.getCurrent().removeWindow(mengNeu);
+							ViewHandler.getInstance().switchView(KategorienAnzeigen.class);
+						}
+					});
+
+
+			        nameUp.addValueChangeListener(new ValueChangeListener() {
+
+			            public void valueChange(final ValueChangeEvent event) {
+			                final String valueString = String.valueOf(event.getProperty()
+			                        .getValue());
+
+			                nameText = valueString;
+			            }
+			        });
+			        
+				}
+				
+			}
+		});
+		
+		
+		
+		
+		
 		layout.addComponent(table);
 		layout.setComponentAlignment(table, Alignment.MIDDLE_CENTER);
 		layout.addComponent(hinzufuegen);
@@ -68,7 +187,84 @@ public class KategorienAnzeigen  extends VerticalLayout  implements View{
 		hinzufuegen.addClickListener(new ClickListener() {			
 			@Override
 			public void buttonClick(ClickEvent event) {
-				ViewHandler.getInstance().switchView(KategorieErstellen.class);				
+				final Window mengNeu = new Window();
+				mengNeu.setClosable(false);
+				mengNeu.setWidth("400px");
+				mengNeu.setHeight("270px");
+				mengNeu.setModal(true);
+				mengNeu.center();
+				mengNeu.setResizable(false);
+				mengNeu.setCaption("Kategorie hinzufügen");
+				
+				UI.getCurrent().addWindow(mengNeu);
+				
+				VerticalLayout	layout = new VerticalLayout();
+				layout.setMargin(true);
+				layout.setWidth("100%");
+				layout.setSpacing(true);
+
+				Button			speichern = new Button(IConstants.BUTTON_SAVE);
+				Button			verwerfen = new Button(IConstants.BUTTON_DISCARD);
+				
+				name.setWidth("100%");
+
+				VerticalLayout feld = new VerticalLayout();
+			
+				feld.addComponent(name);
+
+				HorizontalLayout control = new HorizontalLayout();
+				control.setSpacing(true);
+				control.addComponent(verwerfen);
+				control.addComponent(speichern);
+				speichern.setIcon(new ThemeResource(IConstants.BUTTON_SAVE_ICON));
+				verwerfen.setIcon(new ThemeResource(IConstants.BUTTON_DISCARD_ICON));
+
+				layout.addComponent(feld);
+				layout.setComponentAlignment(feld, Alignment.MIDDLE_CENTER);
+				layout.addComponent(control);
+				layout.setComponentAlignment(control, Alignment.BOTTOM_RIGHT);
+				mengNeu.setContent(layout);
+				
+				name.setImmediate(true);
+				name.setMaxLength(45);
+				name.addValidator(new StringLengthValidator("Bitte gültigen Namen eingeben", 4,45, false));
+				
+				verwerfen.addClickListener(new ClickListener() {
+					
+					@Override
+					public void buttonClick(ClickEvent event) {
+						UI.getCurrent().removeWindow(mengNeu);
+						ViewHandler.getInstance().switchView(KategorienAnzeigen.class);							
+					}
+				});
+
+				speichern.addClickListener(new ClickListener()
+				{
+					public void buttonClick(ClickEvent event)
+					{
+						Kategorie me = new Kategorie();
+						me.setName(nameText);
+						try {
+							Kategorienverwaltung.getInstance().createNewKategorie(me);
+						} catch (ConnectException | DAOException | SQLException e) {
+							throw new NullPointerException("Bitte gültige Werte eingeben");
+						}
+						UI.getCurrent().removeWindow(mengNeu);
+						ViewHandler.getInstance().switchView(KategorienAnzeigen.class);
+					}
+				});
+
+
+		        name.addValueChangeListener(new ValueChangeListener() {
+
+		            public void valueChange(final ValueChangeEvent event) {
+		                final String valueString = String.valueOf(event.getProperty()
+		                        .getValue());
+
+		                nameText = valueString;
+		            }
+		        });
+		        
 			}
 		});
 	}
