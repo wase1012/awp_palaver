c95039a4cdc1c51298ba4d27777693c5734f2a5d
Christine Hartkorn <hach1014@hs-karlsruhe.de>
2013-05-13 10:37:09 +0200

    Menüleisten-Bezeichnungen geändert

--- palaver2/src/main/java/de/bistrosoft/palaver/util/IConstants.java
+++ palaver2/src/main/java/de/bistrosoft/palaver/util/IConstants.java
@@ -18,12 +18,12 @@ public interface IConstants {
 	public final String MENU_BESTELLUNG_HEADLINE = "Bestellung";
 
 	public final String MENU_REZEPT_HEADLINE = "Rezept";
-	public final String MENU_REZEPT_NEU = "Anlegen";
-	public final String MENU_REZEPT_ANZEIGEN = "Anzeigen";
-	public final String MENU_REZEPT_AENDERN = "Ändern";
-	public final String MENU_REZEPT_LOESCHEN = "Löschen";
+	public final String MENU_REZEPT_NEU = "Rezept Anlegen";
+	public final String MENU_REZEPT_ANZEIGEN = "Rezept Anzeigen";
+	public final String MENU_REZEPT_AENDERN = "Rezept Ändern";
+	public final String MENU_REZEPT_LOESCHEN = "Rezept Löschen";
 	public final String MENU_MENUE_HEADLINE = "Menü";
-	public final String MENU_MENUE_ANLEGEN = " M Anlegen";
+	public final String MENU_MENUE_ANLEGEN = " Menü Anlegen";
 	public final String MENU_MENUPLAN_HEADLINE = "Menüplan";
 	public final String MENU_MENUPLAN_AKTUELL = "Aktueller Menüplan";
 	public final String MENU_MENUPLAN_HISTORIE = "Menüplan-Historie";
