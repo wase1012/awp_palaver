6a16fcc4a8eca243bd6f1675958a073f0c1bc9f9
bomi1012 <bomi1012@hs-karlsruhe.de>
2013-05-17 14:10:34 +0200

    Emailversand benötigte jars: activation-1.1.1.jar, mail.jar

---
+++ palaver/src/main/java/de/hska/awp/palaver/emailversand/MailAuthenticator.java
@@ -0,0 +1,31 @@
+package de.hska.awp.palaver.emailversand;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+/**
+ * @author Mihail Boehm
+ */
+public class MailAuthenticator extends Authenticator{
+	private String 		user;
+    private String 		password;
+     
+    /**
+     * Benutzername und Passwort prüfen
+     * @param user
+     * @param password
+     */
+    public MailAuthenticator(String user, String password)
+    {
+        this.user = user;
+        this.password = password;
+    }
+     
+    /**
+     * getPasswordAuthentication() wird automatisch aufgerufen
+     * sobald der Benutzernamen + Passwort verlangt wird
+     */
+    public PasswordAuthentication getPasswordAuthentication()
+    {
+        return new PasswordAuthentication(user, password);
+    }
+}
