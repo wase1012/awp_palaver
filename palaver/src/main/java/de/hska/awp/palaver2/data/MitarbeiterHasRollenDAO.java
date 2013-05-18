d1541e52ac0517fbe953bf5eb1f33a8790c407e9
bach1014 <barth_chris@hotmail.com>
2013-05-18 10:35:53 +0200

    Update MitarbeiterHasRollenDAO

--- palaver/src/main/java/de/hska/awp/palaver2/data/MitarbeiterHasRollenDAO.java
+++ palaver/src/main/java/de/hska/awp/palaver2/data/MitarbeiterHasRollenDAO.java
@@ -6,7 +6,6 @@ import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.hska.awp.palaver2.mitarbeiterverwaltung.domain.Mitarbeiter;
 import de.hska.awp.palaver2.mitarbeiterverwaltung.domain.MitarbeiterHasRollen;
 
 public class MitarbeiterHasRollenDAO extends AbstractDAO{
@@ -14,12 +13,9 @@ public class MitarbeiterHasRollenDAO extends AbstractDAO{
 	private static MitarbeiterHasRollenDAO instance = null;
 
 	private final static String TABLE = "mitarbeiter_has_rollen";
-//	private final static String ID = "id";
 	private final static String MITARBEITER_FK = "mitarbeiter_fk";
 	private final static String ROLLEN_FK = "rollen_fk";
-	
 	private final static String GET_ALL_MITARBEITER_HAS_ROLLEN = "SELECT * FROM mitarbeiter_has_rollen";
-	private final static String DELETE_MITARBEITER_HAS_ROLLEN = "DELETE FROM mitarbeiter_has_rollen WHERE mitarbeiter_has_rollen.id= {0}";
 	private final static String DELETE = "DELETE FROM mitarbeiter_has_rollen WHERE mitarbeiter_has_rollen.mitarbeiter_fk = {0} AND mitarbeiter_has_rollen.rollen_fk = {1}";
 	
 	
@@ -54,28 +50,7 @@ public class MitarbeiterHasRollenDAO extends AbstractDAO{
 				+ "VALUES" + "('" + mitarbeiterHasRollen.getMitarbeiter_fk() + "','"
 				+ mitarbeiterHasRollen.getRollen_fk() + "')";
 		this.putManaged(INSERT_QUERY);
-	}
-	
-//	public void updateMitarbeiterHasRollen(MitarbeiterHasRollen mitarbeiterHasRollen) throws ConnectException,
-//		DAOException, SQLException {
-//		String UPDATE_QUERY = "UPDATE " + TABLE + " SET " + MITARBEITER_FK + "='"
-//				+ mitarbeiterHasRollen.getMitarbeiter_fk() + "'," + ROLLEN_FK + "='"
-//				+ mitarbeiterHasRollen.getRollen_fk()
-//			    + "' WHERE " + ID + "='" + mitarbeiterHasRollen.getId() + "'";
-//		this.putManaged(UPDATE_QUERY);
-//	}
-	
-	
-	
-//	public void deleteMitarbeiterHasRollen(Long id) throws ConnectException,
-//			DAOException, SQLException {
-//
-//		if (id == null) {
-//			throw new NullPointerException("keine MitarbeiterHasRollen ID übergeben");
-//		}
-//		putManaged(MessageFormat.format(DELETE_MITARBEITER_HAS_ROLLEN, id));
-//	}
-	
+	}	
 	
 	public void deleteMitarbeiterHasRollen(Long idmitarbeiter, Long idrolle) throws ConnectException,
 			DAOException, SQLException {
