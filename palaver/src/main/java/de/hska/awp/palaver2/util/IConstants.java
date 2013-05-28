7e2b5177f389c7138d6b26573f5d7bd2855e6a31
wase1012 <wase1012@hs-karlsruhe.de>
2013-05-28 20:03:33 +0200

    POM!!!

--- palaver/src/main/java/de/hska/awp/palaver2/util/IConstants.java
+++ palaver/src/main/java/de/hska/awp/palaver2/util/IConstants.java
@@ -9,74 +9,74 @@ public interface IConstants
 	//1433
 	//3306
 	// DB Verbindung
-	public final String		DB_CONNECTION_URL = "jdbc:mysql://localhost:1433/palaver?user=root&password=root";
+	String		DB_CONNECTION_URL = "jdbc:mysql://localhost:1433/palaver?user=root&password=root";
 	
 	// Menuepunkte mit Unterpunkten
-	public final String		MENU_ARTIKEL_HEADLINE = "Artikel";
-	public final String		MENU_ARTIKEL_NEU = "Artikel anlegen";
-	public final String		MENU_ARTIKEL_ANZEIGEN = "Alle Artikel";
-	public final String		MENU_LIEFERANT_HEADLINE = "Lieferant";
-	public final String		MENU_LIEFERANT_NEW = "Lieferant anlegen";
-	public final String		MENU_LIEFERANT_ANZEIGEN = "Alle Lieferanten";
-	public final String		MENU_EINSTELLUNGEN_HEADLINE = "Einstellungen";
-	public final String		MENU_BESTELLUNG_HEADLINE = "Bestellung";
-	public final String		MENU_BESTELLUNG_NEW_RANDOM = "Neue manuelle Bestellung";
-	public final String		MENU_BESTELLUNG_NEW = "Neue Bestellung";
-	public final String 	MENU_BESTELLUNG_BEARBEITEN = "Bestellung bearbeiten";
-	public final String 	MENU_BESTELLUNG_ANZEIGEN = "Alle Bestellungen";
-	public final String		MENU_NACHRICHT_ANZEIGEN = "Alle Nachrichten";
-	public final String		MENU_NACHRICHT_NEU = "Nachrichten erstellen";
-	public final String		MENU_NACHRICHT_HEADLINE = "Nachrichten";
-	public final String		MENU_MENGENEINHEIT_ANZEIGEN = "Alle Mengeneinheiten";
-	public final String		MENU_MENGENEINHEIT_NEU = "Mengeneinheit anlegen";
-	public final String		MENU_KATEGORIE_ANZEIGEN = "Alle Kategorien";
-	public final String 	MENU_REZEPT_HEADLINE = "Rezept";
-	public final String 	MENU_REZEPT_NEU = "Anlegen";
-	public final String 	MENU_REZEPT_ANZEIGEN = "Anzeigen";
-	public final String 	MENU_REZEPT_AENDERN = "Ã?ndern";
-	public final String 	MENU_REZEPT_LOESCHEN = "Löschen";
-	public final String 	MENU_MENUPLAN_HEADLINE = "Menüplan";
-	public final String 	MENU_MENUPLAN_AKTUELL = "Aktueller Menüplan";
-	public final String 	MENU_MENUPLAN_HISTORIE = "Menüplan-Historie";
-	public final String 	MENU_FUSSNOTE = "Fussnoten";
-	public final String 	MENU_GESCHMACK = "Geschmäcker";
-	public final String 	MENU_REZEPTART = "Rezeptarten";
-	public final String 	MENU_ZUBEREITUNG = "Zubereitungen";
-	public final String		MENU_LOGOUT = "Logout";
-	public final String 	MENU_MENUE_HEADLINE = "Menü";
-	public final String 	MENU_MENUE_ANLEGEN = " Menü anlegen";
-	public final String 	MENU_MENUE_SUCHEN = " Menü suchen";
-	public final String 	MENU_REGEL = "Regeln";
+	String		MENU_ARTIKEL_HEADLINE = "Artikel";
+	String		MENU_ARTIKEL_NEU = "Artikel anlegen";
+	String		MENU_ARTIKEL_ANZEIGEN = "Alle Artikel";
+	String		MENU_LIEFERANT_HEADLINE = "Lieferant";
+	String		MENU_LIEFERANT_NEW = "Lieferant anlegen";
+	String		MENU_LIEFERANT_ANZEIGEN = "Alle Lieferanten";
+	String		MENU_EINSTELLUNGEN_HEADLINE = "Einstellungen";
+	String		MENU_BESTELLUNG_HEADLINE = "Bestellung";
+	String		MENU_BESTELLUNG_NEW_RANDOM = "Neue manuelle Bestellung";
+	String		MENU_BESTELLUNG_NEW = "Neue Bestellung";
+	String 	MENU_BESTELLUNG_BEARBEITEN = "Bestellung bearbeiten";
+	String 	MENU_BESTELLUNG_ANZEIGEN = "Alle Bestellungen";
+	String		MENU_NACHRICHT_ANZEIGEN = "Alle Nachrichten";
+	String		MENU_NACHRICHT_NEU = "Nachrichten erstellen";
+	String		MENU_NACHRICHT_HEADLINE = "Nachrichten";
+	String		MENU_MENGENEINHEIT_ANZEIGEN = "Alle Mengeneinheiten";
+	String		MENU_MENGENEINHEIT_NEU = "Mengeneinheit anlegen";
+	String		MENU_KATEGORIE_ANZEIGEN = "Alle Kategorien";
+	String 	MENU_REZEPT_HEADLINE = "Rezept";
+	String 	MENU_REZEPT_NEU = "Anlegen";
+	String 	MENU_REZEPT_ANZEIGEN = "Anzeigen";
+	String 	MENU_REZEPT_AENDERN = "Ã?ndern";
+	String 	MENU_REZEPT_LOESCHEN = "Löschen";
+	String 	MENU_MENUPLAN_HEADLINE = "Menüplan";
+	String 	MENU_MENUPLAN_AKTUELL = "Aktueller Menüplan";
+	String 	MENU_MENUPLAN_HISTORIE = "Menüplan-Historie";
+	String 	MENU_FUSSNOTE = "Fussnoten";
+	String 	MENU_GESCHMACK = "Geschmäcker";
+	String 	MENU_REZEPTART = "Rezeptarten";
+	String 	MENU_ZUBEREITUNG = "Zubereitungen";
+	String		MENU_LOGOUT = "Logout";
+	String 	MENU_MENUE_HEADLINE = "Menü";
+	String 	MENU_MENUE_ANLEGEN = " Menü anlegen";
+	String 	MENU_MENUE_SUCHEN = " Menü suchen";
+	String 	MENU_REGEL = "Regeln";
 	
 	// Beschriftungen fuer Knoepfe
-	public final String		BUTTON_SHOW_FILTER = "Filter anzeigen";
-	public final String		BUTTON_HIDE_FILTER = "Filter ausblenden";
-	public final String		BUTTON_SAVE = "Speichern";
-	public final String		BUTTON_SENDEN = "Senden";
-	public final String		BUTTON_DISCARD = "Verwerfen";
-	public final String		BUTTON_ADD = "Hinzufügen";
-	public final String		BUTTON_NEW = "Neu";
-	public final String		BUTTON_EDIT = "Bearbeiten";
-	public final String		BUTTON_OK = "OK";
-	public final String		BUTTON_LÖSCHEN = "Löschen";
-	public final String		BUTTON_JA = "Ja";
-	public final String 	BUTTON_NEIN = "Nein";
-	public final String		BUTTON_EMAILVERSAND = "Per Email versenden";
-	public final String		BUTTON_DOWNLOAD = "Download";
+	String		BUTTON_SHOW_FILTER = "Filter anzeigen";
+	String		BUTTON_HIDE_FILTER = "Filter ausblenden";
+	String		BUTTON_SAVE = "Speichern";
+	String		BUTTON_SENDEN = "Senden";
+	String		BUTTON_DISCARD = "Verwerfen";
+	String		BUTTON_ADD = "Hinzufügen";
+	String		BUTTON_NEW = "Neu";
+	String		BUTTON_EDIT = "Bearbeiten";
+	String		BUTTON_OK = "OK";
+	String		BUTTON_LÖSCHEN = "Löschen";
+	String		BUTTON_JA = "Ja";
+	String 	BUTTON_NEIN = "Nein";
+	String		BUTTON_EMAILVERSAND = "Per Email versenden";
+	String		BUTTON_DOWNLOAD = "Download";
 	
 	// Icons fuer Knoepfe
-	public final String		BUTTON_DELETE_ICON = "img/delete.ico";
-	public final String		BUTTON_SAVE_ICON = "img/save.ico";
-	public final String		BUTTON_DISCARD_ICON = "img/cross.ico";
-	public final String 	BUTTON_ADD_ICON = "img/add.ico";
-	public final String 	BUTTON_NEW_ICON = "img/new.ico";
-	public final String 	BUTTON_EDIT_ICON = "img/edit.ico";
-	public final String		BUTTON_OK_ICON = "img/check.ico";
-//	public final String		BUTTON_EMAILVERSAND_ICON = "";
+	String		BUTTON_DELETE_ICON = "img/delete.ico";
+	String		BUTTON_SAVE_ICON = "img/save.ico";
+	String		BUTTON_DISCARD_ICON = "img/cross.ico";
+	String 	BUTTON_ADD_ICON = "img/add.ico";
+	String 	BUTTON_NEW_ICON = "img/new.ico";
+	String 	BUTTON_EDIT_ICON = "img/edit.ico";
+	String		BUTTON_OK_ICON = "img/check.ico";
+//	String		BUTTON_EMAILVERSAND_ICON = "";
 	
-	public final String		ICON_YES = "img/tick_circle.ico";
-	public final String		ICON_NO = "img/cross_circle.ico";
-	public final String		ICON_DELETE = "img/Delete.ico";
+	String		ICON_YES = "img/tick_circle.ico";
+	String		ICON_NO = "img/cross_circle.ico";
+	String		ICON_DELETE = "img/Delete.ico";
 
 	
 
