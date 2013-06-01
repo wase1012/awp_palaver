e279b9572b608ba287d5da6cfbab2fd7e8dd49d9
bach1014 <barth_chris@hotmail.com>
2013-06-01 13:36:32 +0200

    Checkstyle

--- palaver/src/main/java/de/hska/awp/palaver2/popup/AnsprechpartnerAdd.java
+++ palaver/src/main/java/de/hska/awp/palaver2/popup/AnsprechpartnerAdd.java
@@ -1,7 +1,5 @@
 package de.hska.awp.palaver2.popup;
 
-import java.sql.SQLException;
-
 import com.vaadin.annotations.Theme;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
@@ -16,8 +14,6 @@ import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
-import de.hska.awp.palaver2.data.ConnectException;
-import de.hska.awp.palaver2.data.DAOException;
 import de.hska.awp.palaver2.lieferantenverwaltung.domain.Ansprechpartner;
 import de.hska.awp.palaver2.lieferantenverwaltung.domain.Lieferant;
 import de.hska.awp.palaver2.lieferantenverwaltung.service.Ansprechpartnerverwaltung;
@@ -27,46 +23,45 @@ import de.hska.awp.palaver2.util.IConstants;
 /**
  * 
  * @author PhilippT
- *
+ * 
  */
 
 @Theme("palaver")
 @SuppressWarnings("serial")
 public class AnsprechpartnerAdd extends UI {
 
-	private VerticalLayout	box = new VerticalLayout();
-	
-	private TextField		name = new TextField("Name");
-	private TextField		telefon = new TextField("Telefon");
-	private TextField		handy = new TextField("Handy");
-	private TextField		fax = new TextField("Fax");
-	
-	private String			nameInput;
-	private String			telefonInput;
-	private String 			handyInput;
-	private String 			faxInput;
-	private Lieferant 		lieferant;
+	private VerticalLayout box = new VerticalLayout();
 
-	private Button			speichern = new Button(IConstants.BUTTON_SAVE);
-	private Button			verwerfen = new Button(IConstants.BUTTON_DISCARD);
+	private TextField name = new TextField("Name");
+	private TextField telefon = new TextField("Telefon");
+	private TextField handy = new TextField("Handy");
+	private TextField fax = new TextField("Fax");
 
+	private String nameInput;
+	private String telefonInput;
+	private String handyInput;
+	private String faxInput;
+	private Lieferant lieferant;
+
+	private Button speichern = new Button(IConstants.BUTTON_SAVE);
+	private Button verwerfen = new Button(IConstants.BUTTON_DISCARD);
 
 	@Override
-    protected void init(VaadinRequest request) {
-        getPage().setTitle("Ansprechpartner hinzufügen");
-        
-        setContent(box);
+	protected void init(VaadinRequest request) {
+		getPage().setTitle("Ansprechpartner hinzufügen");
+
+		setContent(box);
 		this.setSizeFull();
-		
+
 		name.setWidth("100%");
 		telefon.setWidth("100%");
 		handy.setWidth("100%");
 		fax.setWidth("100%");
-		
+
 		VerticalLayout feld = new VerticalLayout();
-		
+
 		box.setSpacing(true);
-	
+
 		feld.addComponent(name);
 		feld.addComponent(telefon);
 		feld.addComponent(handy);
@@ -79,39 +74,37 @@ public class AnsprechpartnerAdd extends UI {
 		control.setSpacing(true);
 		box.setWidth("300px");
 		box.addComponent(control);
-		box.setComponentAlignment(control, Alignment.BOTTOM_RIGHT);	
+		box.setComponentAlignment(control, Alignment.BOTTOM_RIGHT);
 		control.addComponent(verwerfen);
 		control.addComponent(speichern);
 		speichern.setIcon(new ThemeResource(IConstants.BUTTON_SAVE_ICON));
 		verwerfen.setIcon(new ThemeResource(IConstants.BUTTON_DISCARD_ICON));
-		
+
 		name.setImmediate(true);
 		name.setInputPrompt(nameInput);
 		name.setMaxLength(15);
-		
+
 		telefon.setImmediate(true);
 		telefon.setInputPrompt(telefonInput);
-		telefon.setMaxLength(10);	
-		
+		telefon.setMaxLength(10);
+
 		handy.setImmediate(true);
 		handy.setInputPrompt(handyInput);
 		handy.setMaxLength(10);
-		
+
 		fax.setImmediate(true);
 		fax.setInputPrompt(faxInput);
 		fax.setMaxLength(10);
-		
+
 		try {
 			lieferant = Lieferantenverwaltung.getInstance().getLastLieferant();
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		  
-		speichern.addClickListener(new ClickListener()
-		{
-			public void buttonClick(ClickEvent event)
-			{
+
+		speichern.addClickListener(new ClickListener() {
+			public void buttonClick(ClickEvent event) {
 				Ansprechpartner ans = new Ansprechpartner();
 				ans.setName(nameInput);
 				ans.setTelefon(telefonInput);
@@ -122,52 +115,47 @@ public class AnsprechpartnerAdd extends UI {
 					Ansprechpartnerverwaltung.getInstance().createAnsprechpartner(ans);
 				} catch (Exception e) {
 					throw new NullPointerException("Bitte gültige Werte eingeben");
-				}				
+				}
 			}
 		});
 
-        name.addValueChangeListener(new ValueChangeListener() {
+		name.addValueChangeListener(new ValueChangeListener() {
 
-            public void valueChange(final ValueChangeEvent event) {
-                final String valueString = String.valueOf(event.getProperty()
-                        .getValue());
+			public void valueChange(final ValueChangeEvent event) {
+				final String valueString = String.valueOf(event.getProperty().getValue());
 
-                nameInput = valueString;
-            }
-        });
-        
-        telefon.addValueChangeListener(new ValueChangeListener() {
-            @Override
-            public void valueChange(final ValueChangeEvent event) {
-                final String valueString = String.valueOf(event.getProperty()
-                        .getValue());
-                telefonInput = valueString;
-            }
-        });
-        
-        handy.addValueChangeListener(new ValueChangeListener() {
+				nameInput = valueString;
+			}
+		});
 
-            public void valueChange(final ValueChangeEvent event) {
-                final String valueString = String.valueOf(event.getProperty()
-                        .getValue());
+		telefon.addValueChangeListener(new ValueChangeListener() {
+			@Override
+			public void valueChange(final ValueChangeEvent event) {
+				final String valueString = String.valueOf(event.getProperty().getValue());
+				telefonInput = valueString;
+			}
+		});
 
-                handyInput = valueString;
-            }
-        });
-        
-        fax.addValueChangeListener(new ValueChangeListener() {
+		handy.addValueChangeListener(new ValueChangeListener() {
 
-            public void valueChange(final ValueChangeEvent event) {
-                final String valueString = String.valueOf(event.getProperty()
-                        .getValue());
+			public void valueChange(final ValueChangeEvent event) {
+				final String valueString = String.valueOf(event.getProperty().getValue());
 
-                faxInput = valueString;
-            }
-        });
-    }
+				handyInput = valueString;
+			}
+		});
 
-	public AnsprechpartnerAdd()
-	{
+		fax.addValueChangeListener(new ValueChangeListener() {
+
+			public void valueChange(final ValueChangeEvent event) {
+				final String valueString = String.valueOf(event.getProperty().getValue());
+
+				faxInput = valueString;
+			}
+		});
+	}
+
+	public AnsprechpartnerAdd() {
 		super();
 
 	}
