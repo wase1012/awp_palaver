b7d8127f52d0c2efbbba6b926ebb760e862e8119
Eike Becher <mail@ebecher.de>
2013-06-03 16:41:34 +0200

    Bugfix

--- palaver/src/main/java/de/bistrosoft/palaver/regelverwaltung/domain/Regel.java
+++ palaver/src/main/java/de/bistrosoft/palaver/regelverwaltung/domain/Regel.java
@@ -172,11 +172,14 @@ public class Regel {
 //		} else if (regeltyp.equals("Menueart")) {
 //			mc.addFehlerRegel(checkMenueart(mc, mp));
 //		}
-		if (this.getZeilen().indexOf(mc.row) >= 0 || this.getZeilen().indexOf(-1) >= 0) {
-			if (this.getSpalten().indexOf(mc.col) >= 0 || this.getSpalten().indexOf(-1) >= 0) {
-				this.findeRegel(mc,mp);
+		if(getZeilen()!=null&&getSpalten()!=null){
+			if (this.getZeilen().indexOf(mc.row) >= 0 || this.getZeilen().indexOf(-1) >= 0) {
+				if (this.getSpalten().indexOf(mc.col) >= 0 || this.getSpalten().indexOf(-1) >= 0) {
+					this.findeRegel(mc,mp);
+				}
 			}
 		}
+		
 
 	}
 	
