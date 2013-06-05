242304e80ad82de8b06accfc3f0e3f37c5785dbd
bach1014 <barth_chris@hotmail.com>
2013-06-05 09:23:05 +0200

    Änderung für Team 1 erledigt.

--- palaver/src/main/java/de/hska/awp/palaver2/mitarbeiterverwaltung/domain/Mitarbeiter.java
+++ palaver/src/main/java/de/hska/awp/palaver2/mitarbeiterverwaltung/domain/Mitarbeiter.java
@@ -157,9 +157,51 @@ public class Mitarbeiter implements java.io.Serializable {
 	}
 
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((benutzername == null) ? 0 : benutzername.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((vorname == null) ? 0 : vorname.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Mitarbeiter other = (Mitarbeiter) obj;
+		if (benutzername == null) {
+			if (other.benutzername != null)
+				return false;
+		} else if (!benutzername.equals(other.benutzername))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (vorname == null) {
+			if (other.vorname != null)
+				return false;
+		} else if (!vorname.equals(other.vorname))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
-		return "Mitarbeiter [id=" + id + ", name=" + name + ", vorname=" + vorname + ", email=" + email + ", passwort=" + passwort
-				+ ", eintrittsdatum=" + eintrittsdatum + ", austrittsdatum=" + austrittsdatum + "]";
+		return vorname;
 	}
 
 }
